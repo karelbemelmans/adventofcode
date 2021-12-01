1443c91705aa5cf080a288f11188b34839741855
Karel Bemelmans <mail@karelbemelmans.com>
2021-12-01 07:50:13 +0100

    Added 2021 day 1

---
+++ 2021/01/01.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+with open('input.txt', 'r') as fp:
+    lines = list([int(x) for x in fp.read().splitlines()])
+
+# Part 1
+increased = 0
+for i in range(1, len(lines)):
+    if lines[i] > lines[i-1]:
+        increased = increased + 1
+
+print increased
+
+# Part 2
+increased = 0
+for i in range(3, len(lines)):
+    A = (lines[i-1] + lines[i-2] + lines[i-3])
+    B = (lines[i] + lines[i-1] + lines[i-2])
+    if B > A:
+        increased = increased + 1
+
+print increased
