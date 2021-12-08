acaf251d439e3d468b6b7aef26985b88484bc489
Karel Bemelmans <karel@narfum.org>
2021-12-08 18:52:51 +0100

    Added 2021, day 8. This was fucking hard.

---
+++ 2021/08/08.py
@@ -0,0 +1,164 @@
+from collections import defaultdict
+
+display = {
+    0: 'abcegf',
+    1: 'cf',
+    2: 'acdeg',
+    3: 'acdfg',
+    4: 'bcdf',
+    5: 'abdfg',
+    6: 'abdefg',
+    7: 'acf',
+    8: 'abcdefg',
+    9: 'abcdfg',
+}
+
+def parse_line(line, counter, p2=False):
+    start, end = line.split(" | ")
+    global original
+
+    # p2: Map
+    if p2:
+        words = start.split(" ")
+        patterns = defaultdict(list)
+        new_dict = {}
+
+        # Part 1: Process the obvious ones
+        #         Map all the words into an array grouped by word length
+        for word in words:
+            l = len(word)
+
+            # We have 4 that we can already map to the new_dicht
+            # Number 1
+            if l == 2:
+                patterns[1] = list(word)
+                new_dict[1] = "".join(sorted(word))
+
+            # Number 7
+            elif l == 3:
+                patterns[7] = list(word)
+                new_dict[7] = "".join(sorted(word))
+
+            # Number 4
+            elif l == 4:
+                patterns[4] = list(word)
+                new_dict[4] = "".join(sorted(word))
+
+            # Number 8
+            elif l == 7:
+                patterns[8] = list(word)
+                new_dict[8] = "".join(sorted(word))
+
+            # The 6 next ones we will look at more closely later, for now we just group them
+
+            # Length 5: 2, 3, 5
+            elif l == 5:
+                patterns[235].append(list("".join(sorted(word))))
+
+            # Length 6: 0, 6, 9
+            elif l == 6:
+                patterns[69].append(list("".join(sorted(word))))
+
+        # Part 2
+        #
+        # We measure the amount of differences between digits we know and not know yet
+        # For every digit there is one that has a different set of differences
+
+        ################################################################################################################
+        # Words of length 5
+
+        ## Pattern 2 - Pattern 4 = 3 lines left
+        ## Pattern 3 - Pattern 4 = 2 lines left
+        ## Pattern 5 - Pattern 4 = 2 lines left
+        for pattern in patterns[235]:
+            diff = set(pattern) - set(patterns[4])
+            if len(diff) == 3:
+                new_dict[2] = "".join(sorted(pattern))
+                patterns[235].remove(pattern)
+
+        ## Pattern 3 - Pattern 7 = 2 lines left
+        ## Pattern 5 - Pattern 7 = 3 lines left
+        assert len(patterns[235]) == 2
+        for pattern in patterns[235]:
+            diff = set(pattern) - set(patterns[7])
+            if len(diff) == 2:
+                new_dict[3] = "".join(sorted(pattern))
+                patterns[235].remove(pattern)
+
+        # We have one number left now, that is 5
+        assert len(patterns[235]) == 1
+        new_dict[5] = "".join(patterns[235][0])
+
+
+        ################################################################################################################
+        # Words of length 6
+
+        ## Pattern 0 - Pattern 4 = 3 lines left
+        ## Pattern 6 - Pattern 4 = 3 lines left
+        ## Pattern 9 - Pattern 4 = 2 lines left
+        for pattern in patterns[69]:
+            diff = set(pattern) - set(patterns[4])
+            if len(diff) == 2:
+                new_dict[9] = "".join(sorted(pattern))
+                patterns[69].remove(pattern)
+
+        ## Pattern 0 - Pattern 5 = 2 lines left
+        ## Pattern 6 - Pattern 5 = 1 line left
+        assert len(patterns[69]) == 2
+        for pattern in patterns[69]:
+            diff = set(pattern) - set(list(new_dict[5]))
+            if len(diff) == 1:
+                new_dict[6] = "".join(sorted(pattern))
+                patterns[69].remove(pattern)
+
+        # We have one number left now, that is 0
+        assert len(patterns[69]) == 1
+        new_dict[0] = "".join(patterns[69][0])
+
+        # Make sure we got all 10 at this point
+        assert len(new_dict) == 10
+
+        # Now use our new_dict to translate the codes into digits and return it as a number.
+        words = end.split(" ")
+        number = ""
+        for word in words:
+            word = "".join(sorted(word))
+            if word in new_dict.values():
+                n = list(new_dict.keys())[list(new_dict.values()).index(word)]
+                number += f"{n}"
+
+        return int(number)
+
+    # P1
+    else:
+        for word in end.split(" "):
+            if len(word) in [2, 4, 3, 7]:
+                counter[len(word)] += 1
+
+
+def parse_file(file, p2=False):
+    with open(file, 'r') as fp:
+        lines = [x for x in fp.read().splitlines()]
+
+    counter = defaultdict(int)
+    count = 0
+    for line in lines:
+        if p2:
+            # FIXME
+            count += parse_line(line, counter, True)
+        else:
+            parse_line(line, counter)
+
+    if p2:
+        return count
+    else:
+        return sum(counter.values())
+
+
+# Part 1
+assert parse_file('test.txt') == 26
+print("Part 1: ", parse_file('input.txt'))
+
+# Part 2
+assert parse_file('test.txt', True) == 61229
+print("Part 2: ", parse_file('input.txt', True))
