39daaf93e7bdb455e0c180590135e3228df98d76
Karel Bemelmans <karel@narfum.org>
2021-12-10 10:08:52 +0100

    Added 2021, day 10

---
+++ 2021/10/10.py
@@ -0,0 +1,83 @@
+points = {
+    ')': 3,
+    ']': 57,
+    '}': 1197,
+    '>': 25137,
+}
+
+p2points = {
+    '(': 1,
+    '[': 2,
+    '{': 3,
+    '<': 4,
+}
+
+
+def parse_line(nr, line, p2=False):
+    global points, p2points
+
+    openers = ['(', '[', '{', '<']
+    closers = [')', ']', '}', '>']
+
+    stack = []
+    skipline = False
+
+    # Stop at the first incorrect closing character on each corrupted line.
+    for i, char in enumerate(line):
+        if char in openers:
+            stack.append(char)
+
+        elif char in closers:
+            last = stack.pop()
+
+            # Loop over all the chars we need to check
+            for j in range(len(openers)):
+                if last == openers[j] and not char == closers[j]:
+                    skipline = True
+                    if not p2:
+                        return points[char]
+
+    # If we reach this phase, we have a line that is not corrupt but needs proper closing
+    # In p2 we need to fix the string
+    if p2 and not skipline:
+        score = 0
+        while stack:
+            k = stack.pop()
+            score *= 5
+            score += p2points[k]
+
+        return score
+
+    # In p1, if there was no error, we return a score of 0
+    else:
+        return 0
+
+
+def parse_file(file, p2=False):
+    with open(file, 'r') as fp:
+        lines = [x for x in fp.read().splitlines()]
+
+    score = 0
+    scores = []
+
+    for i, line in enumerate(lines):
+        s = parse_line(i + 1, line, p2)
+        if p2 and s > 0:
+            scores.append(s)
+        else:
+            score += s
+
+    if p2:
+        scores = sorted(scores)
+        return scores[(len(scores) - 1) // 2]
+    else:
+        return score
+
+
+# Part 1
+assert parse_file('test.txt') == 26397
+print("Part 1: ", parse_file('input.txt'))
+
+# Part 2
+assert parse_file('test.txt', True) == 288957
+print("Part 2: ", parse_file('input.txt', True))
