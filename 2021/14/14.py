cdbf053b31ba09b29c12a438f5e0ef3a8c34b5b5
Karel Bemelmans <mail@karelbemelmans.com>
2021-12-14 21:21:56 +0100

    Code optimization

--- 2021/14/14.py
+++ 2021/14/14.py
@@ -1,29 +1,12 @@
-from collections import defaultdict, Counter, deque
-
-
-# Naive p1 solution
-def step_template(template, rules):
-    new = ""
-
-    # naive approach: Loop over the template list
-    for i in range(len(template)-1):
-        word = template[i] + template[i+1]
-        if word in rules:
-            new += template[i] + rules[word]
-
-    # Append the last char
-    new += template[-1]
-
-    return new
+from collections import Counter
 
 
 # Better p2 solution?
 def step_smarter(template, rules, steps):
-
     # Create tuples
     tuples = Counter()
-    for i in range(len(template)-1):
-        tuples[template[i] + template[i+1]] += 1
+    for i in range(len(template) - 1):
+        tuples[template[i] + template[i + 1]] += 1
 
     for i in range(steps):
 
@@ -55,36 +38,23 @@ def parse_file(file, times=3, p2=False):
         a, b = line.split(" -> ")
         rules[a] = b
 
-    if p2:
-        tuples = step_smarter(template, rules, times)
+    tuples = step_smarter(template, rules, times)
 
-        # Count all the unique chars in each pair
-        count = Counter()
-        for t in tuples:
-            count[t[0]] += tuples[t]
+    # Count all the unique chars in each pair
+    count = Counter()
+    for t in tuples:
+        count[t[0]] += tuples[t]
 
-        # Add one for the last char of the template
-        count[template[-1]] += 1
+    # Add one for the last char of the template
+    count[template[-1]] += 1
 
-        return max(count.values()) - min(count.values())
+    return max(count.values()) - min(count.values())
 
-    # P1
-    else:
-        for i in range(1, times+1):
-            template = step_template(template, rules)
-
-        # Count occurrences
-        count = defaultdict(int)
-        for char in template:
-            count[char] += 1
-
-        return max(count.values()) - min(count.values())
 
 # Part 1
 assert parse_file('test.txt', 10) == 1588
 print("Part 1: ", parse_file('input.txt', 10))
 
 # Part 2
-assert parse_file('test.txt', 10, True) == 1588
 assert parse_file('test.txt', 40, True) == 2188189693529
 print("Part 2: ", parse_file('input.txt', 40, True))
