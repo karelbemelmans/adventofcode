09476e20b925cafc672d9067970d56e3ffe0585e
Karel Bemelmans <mail@karelbemelmans.com>
2021-12-23 17:00:34 +0100

    Added 2021, day 21, part 2. Had to revisit some of last year's dynamic programming code to get this done.

--- 2021/21/21.py
+++ 2021/21/21.py
@@ -1,17 +1,12 @@
 from itertools import cycle
+from collections import Counter
 
-
-# a and b are our start positions
-def play_game(a_pos, b_pos):
+def play_game(a_pos, b_pos, a_score=0, b_score=0, wins=1000):
     dice = cycle(range(1, 101))
-    W = 1000
 
     # We turn 1..10 into 0..9 so we can use % 10
     a_pos -= 1
     b_pos -= 1
-
-    a_score = 0
-    b_score = 0
 
     n = 0
     while True:
@@ -21,7 +16,7 @@ def play_game(a_pos, b_pos):
         a_pos = (a_pos + a_roll) % 10
         a_score += a_pos + 1
 
-        if a_score >= W:
+        if a_score >= wins:
             return b_score * n
 
         b_roll = sum([next(dice) for _ in range(3)])
@@ -30,16 +25,58 @@ def play_game(a_pos, b_pos):
         b_pos = (b_pos + b_roll) % 10
         b_score += b_pos + 1
 
-        if b_score >= W:
+        if b_score >= wins:
             return a_score * n
 
+CACHE = {}
+def dirac_dice(a_pos, b_pos, a_score, b_score):
+    win = 21
 
-def parse_file(file):
+    if a_score >= win:
+        CACHE[(a_pos, b_pos, a_score, b_score)] = 'a'
+        return (1, 0)
+
+    if b_score >= win:
+        CACHE[(a_pos, b_pos, a_score, b_score)] = 'b'
+        return (0, 1)
+
+    # Did we already play this game and know the outcome?
+    if (a_pos, b_pos, a_score, b_score) in CACHE:
+        return CACHE[(a_pos, b_pos, a_score, b_score)]
+
+    score = (0, 0)
+    # We roll 3 times and every time the universe splits
+    for i in [1, 2, 3]:
+        for j in [1, 2, 3]:
+            for k in [1, 2, 3]:
+                new_a_pos = (a_pos + i + j + k) % 10
+                new_a_score = a_score + new_a_pos + 1
+
+                a, b = dirac_dice(b_pos, new_a_pos, b_score, new_a_score)
+                score = (score[0] + b, score[1] + a)
+
+    CACHE[a_pos, b_pos, a_score, b_score] = score
+    return score
+
+
+def parse_file(file, p2=False):
     with open(file, 'r') as fp:
         players = [x for x in fp.read().splitlines()]
 
-    score = play_game(int(players[0][-1]), int(players[1][-1]))
-    return score
+    a_pos = int(players[0][-1])
+    b_pos = int(players[1][-1])
+
+    if p2:
+        # We turn 1..10 into 0..9 so we can use % 10
+        a_pos -= 1
+        b_pos -= 1
+
+        score = dirac_dice(a_pos, b_pos, 0, 0)
+        return max(score)
+
+    else:
+        score = play_game(a_pos, b_pos, 0, 0, 1000)
+        return score
 
 
 # Part 1
@@ -47,5 +84,5 @@ assert parse_file('test.txt') == 739785
 print("Part 1: ", parse_file('input.txt'))
 
 # Part 2
-# assert parse_file('test.txt', 50) == 3351
-# print("Part 1: ", parse_file('input.txt', 50))
+assert parse_file('test.txt', True) == 444356092776315
+print("Part 2: ", parse_file('input.txt', True))
