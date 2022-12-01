926b7b875d702f2405e5ca49e5165c32184df2de
Karel Bemelmans <karel.bemelmans@kry.se>
2022-12-01 19:01:12 +0100

    Added 2022 day 01

---
+++ 2022/01/01.py
@@ -0,0 +1,28 @@
+def parse_file(file, p2=False):
+    with open(file, 'r') as fp:
+        lines = [x for x in fp.read().splitlines()]
+
+    c = []
+    current = 0
+    for line in lines:
+        print (line)
+        if line == "":
+            c.append(current)
+            current = 0
+        else:
+            current += int(line)
+
+    # Make sure we append the last item as well
+    c.append(current)
+
+    if p2:
+        c.sort(reverse=True)
+        return c[0]+c[1]+c[2]
+    else:
+        return max(c)
+
+assert parse_file('test.txt') == 24000
+print("Part 1: ", parse_file('input.txt'))
+
+assert parse_file('test.txt', True) == 45000
+print("Part 2: ", parse_file('input.txt', True))
