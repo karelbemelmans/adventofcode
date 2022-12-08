4a75436b71ffb03229e8208f3c9aef94e04c0bbb
Karel Bemelmans <karel.bemelmans@kry.se>
2022-12-08 17:42:41 +0100

    Completed 2022 day 08

--- 2022/08/08.py
+++ 2022/08/08.py
@@ -1,5 +1,8 @@
 # Pretty much a copy/paste from 2021 day 09
 
+from collections import defaultdict
+
+
 def parse_grid(grid, p2=False):
     R = len(grid)
     C = len(grid[0])
@@ -23,6 +26,7 @@ def parse_grid(grid, p2=False):
                 # Nr of directions we are not visible from
                 count = 0
 
+                # Directions we are looking in
                 for x, y in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
 
                     # We look in increasing radius around our point
@@ -45,9 +49,51 @@ def parse_grid(grid, p2=False):
             if visible:
                 result += 1
 
-    # print("edges: ", edges)
-    # print("result: ", result)
     return result
+
+
+def parse_grid2(grid, p2=False):
+    R = len(grid)
+    C = len(grid[0])
+
+    m = 0
+
+    for r in range(R):
+        for c in range(C):
+
+            # Trees on the edge give always 0, so we skip them
+            if (r in [0, (R-1)]) or (c in [0, (C-1)]):
+                continue
+
+            # We keep the 4 distance scores in this dict
+            distance = defaultdict(int)
+
+            # Directions we are looking in
+            for x, y in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+
+                distance[(x, y)] = 0
+
+                # To measure the viewing distance from a given tree, look up, down, left, and right from that tree;
+                # stop if you reach an edge or at the first tree that is the same height or taller than the tree
+                # under consideration. (If a tree is right on the edge, at least one of its viewing distances will be zero.)
+
+                for l in range(1, max(R, C)):
+                    rr = r + l*x
+                    cc = c + l*y
+
+                    if (0 <= rr < R) and (0 <= cc < C):
+                        # We hit a tree that ends our measuring, but still gives 1 score
+                        if grid[rr][cc] >= grid[r][c]:
+                            distance[(x, y)] += 1
+                            break
+                        else:
+                            distance[(x, y)] += 1
+
+            score = distance[(1, 0)] * distance[(0, 1)] * \
+                distance[(-1, 0)] * distance[(0, -1)]
+            m = max(m, score)
+
+    return m
 
 
 def parse_file(file, p2=False):
@@ -55,7 +101,10 @@ def parse_file(file, p2=False):
         grid = [[int(char) for char in line]
                 for line in fp.read().splitlines()]
 
-    return parse_grid(grid, p2)
+    if p2:
+        return parse_grid2(grid)
+    else:
+        return parse_grid(grid)
 
 
 # Part 1
