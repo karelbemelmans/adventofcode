9ca1868f0ec0947de8fb6bd1051ce95c0baf9a85
Karel Bemelmans <karel.bemelmans@kry.se>
2022-12-09 20:44:31 +0100

    Completed 2022 day 09

--- 2022/09/09.py
+++ 2022/09/09.py
@@ -1,4 +1,23 @@
-from collections import defaultdict
+from copy import deepcopy
+
+
+def show(H, T):
+    R = 10
+    C = 10
+
+    for c in range(C, -C, -1):
+        for r in range(-R, R):
+            if H == [c, r]:
+                print("H", end='')
+            elif [c, r] in T:
+                print(T.index([c, r])+1, end='')
+            elif [c, r] == [0, 0]:
+                print("s", end='')
+            else:
+                print(".", end='')
+
+        print("")
+    print("")
 
 
 def touching(H, T):
@@ -51,31 +70,17 @@ def move_tail(H, T, dir):
     # Otherwise, if the head and tail aren't touching and aren't in the same row or column,
     # the tail always moves one step diagonally to keep up:
     else:
-        match dir:
-            case 'U':
-                T[0] += 1
-                if H[1] > T[1]:
-                    T[1] += 1
-                else:
-                    T[1] -= 1
-            case 'D':
-                T[0] -= 1
-                if H[1] > T[1]:
-                    T[1] += 1
-                else:
-                    T[1] -= 1
-            case 'R':
-                if H[0] > T[0]:
-                    T[0] += 1
-                else:
-                    T[0] -= 1
-                T[1] += 1
-            case 'L':
-                if H[0] > T[0]:
-                    T[0] += 1
-                else:
-                    T[0] -= 1
-                T[1] -= 1
+        # 0
+        if H[0] > T[0]:
+            T[0] += 1
+        else:
+            T[0] -= 1
+
+        # 1
+        if H[1] > T[1]:
+            T[1] += 1
+        else:
+            T[1] -= 1
 
     return T
 
@@ -89,7 +94,6 @@ def parse(lines):
 
     for line in lines:
         (dir, count) = line.strip().split(" ")
-        print("Start:", H, T, dir, count)
 
         for i in range(int(count)):
             match dir:
@@ -102,18 +106,55 @@ def parse(lines):
                 case 'L':
                     H[1] = H[1]-1
 
-            print(" - Moved H", H)
             # After each step, you'll need to update the position of the tail
             # if the step means the head is no longer adjacent to the tail.
             T = move_tail(H, T, dir)
-            print(" -  Moved T", T)
 
             # After simulating the rope, you can count up all of the positions the tail visited at least once.
             visited.add((T[0], T[1]))
 
-        print(" Done:", H, T)
+    return len(visited)
 
-    print("visited", visited, len(visited))
+
+def parse2(lines):
+
+    H = [0, 0]
+    T = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0],
+         [0, 0], [0, 0], [0, 0], [0, 0]]
+
+    # show(H, T)
+
+    visited = set()
+
+    for line in lines:
+        (dir, count) = line.strip().split(" ")
+        # print("Move: ", dir, count)
+        # print(" Start:", H, T)
+
+        for i in range(int(count)):
+            match dir:
+                case 'U':
+                    H[0] = H[0]+1
+                case 'D':
+                    H[0] = H[0]-1
+                case 'R':
+                    H[1] = H[1]+1
+                case 'L':
+                    H[1] = H[1]-1
+
+            # After each step, you'll need to update the position of the tail
+            # if the step means the head is no longer adjacent to the tail.
+            T[0] = move_tail(H, T[0], dir)
+            for i in range(1, len(T)):
+                T[i] = move_tail(T[i-1], T[i], dir)
+
+            # After simulating the rope, you can count up all of the positions the tail visited at least once.
+            visited.add((T[-1][0], T[-1][1]))
+
+        # show(H, T)
+        # print(" Done: ", H, T)
+
+    # print("visited", visited, len(visited))
     return len(visited)
 
 
@@ -121,11 +162,15 @@ def parse_file(file, p2=False):
     with open(file, 'r') as fp:
         lines = [x for x in fp.read().splitlines()]
 
-    return parse(lines)
+    if p2:
+        return parse2(lines)
+    else:
+        return parse(lines)
 
 
 assert parse_file('test.txt') == 13
 print("Part 1: ", parse_file('input.txt'))
 
-# assert parse_file('test.txt', True) == 24933642
-# print("Part 2: ", parse_file('input.txt', True))
+assert parse_file('test.txt', True) == 1
+assert parse_file('test2.txt', True) == 36
+print("Part 2: ", parse_file('input.txt', True))
