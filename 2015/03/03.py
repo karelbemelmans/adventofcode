af15720847d65418b6fa8ad2c1ffecd2fd7b5837
Karel Bemelmans <karel.bemelmans@kry.se>
2022-12-11 16:42:00 +0100

    Added 2015 day 03

---
+++ 2015/03/03.py
@@ -0,0 +1,27 @@
+def parse_file(file, p2=False):
+    with open(file, 'r') as fp:
+        moves = [x for x in fp.read()]
+
+    pos = (0, 0)
+    robot = (0, 0)
+    visited = set()
+    visited.add(pos)
+
+    DIR = {'<': [-1, 0], '>': [1, 0], '^': [0, 1], 'v': [0, -1]}
+
+    for i, move in enumerate(moves):
+        if p2 and i % 2 == 1:
+            robot = (robot[0] + DIR[move][0], robot[1] + DIR[move][1])
+            visited.add(robot)
+        else:
+            pos = (pos[0] + DIR[move][0], pos[1] + DIR[move][1])
+            visited.add(pos)
+
+    return len(visited)
+
+
+print("Part 1: ", parse_file('input.txt'))
+
+assert parse_file('test.txt', True) == 3
+assert parse_file('test2.txt', True) == 11
+print("Part 2: ", parse_file('input.txt', True))
