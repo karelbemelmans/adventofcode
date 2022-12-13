60e12a9068fabc1a09501276e7606ec97060a469
Karel Bemelmans <karel.bemelmans@kry.se>
2022-12-13 09:41:27 +0100

    Added 2022 day 13 complete and cleaned up

--- 2022/13/13.py
+++ 2022/13/13.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import json
 from collections import deque
+from functools import cmp_to_key
 
 
 # Exit codes:
@@ -8,12 +9,9 @@ from collections import deque
 #    0 is continue
 #    1 is done, with fail
 def compare(a, b):
-    print(a, b)
-
-    # We start looking unto we have a case where we need to return False
+    # We start looking until we have a case where we need to return False
 
     if isinstance(a, int) and isinstance(b, int):
-        print(" both int", a, b)
         if a < b:
             return -1
         elif a == b:
@@ -24,7 +22,6 @@ def compare(a, b):
         # In other cases we continue
 
     elif isinstance(a, list) and isinstance(b, list):
-        print(" both list")
         L = min(len(a), len(b))
         for i in range(L):
             c = compare(a[i], b[i])
@@ -34,11 +31,9 @@ def compare(a, b):
                 return 1
 
         if len(a[L:]) == 0 and len(b[L:]) > 0:
-            print(" a remainder is empty")
             return -1
 
         elif len(b[L:]) == 0 and len(a[L:]) > 0:
-            print(" b remainder is empty")
             return 1
 
         # We continue
@@ -46,42 +41,61 @@ def compare(a, b):
             return 0
 
     elif isinstance(a, int) and not isinstance(b, int):
-        print(" a int, not b")
         return compare([a], b)
 
     elif not isinstance(a, int) and isinstance(b, int):
-        print(" b int, not a")
         return compare(a, [b])
 
 
 def parse_file(file, p2=False):
-    with open(file, 'r') as fp:
-        pairs = [[y for y in x.split("\n")]
-                 for x in fp.read().split("\n\n")]
 
-    result = 0
-    for i, pair in enumerate(pairs):
-        left, right = pair
-        print(f"Pair %d:" % (i+1), left, right)
+    if p2:
+        with open(file, 'r') as fp:
+            data = [x.strip() for x in fp.read().splitlines()]
 
-        dl = json.loads(left)
-        dr = json.loads(right)
+        # We can do this better...
+        lines = []
+        for line in data:
+            if not line == "":
+                lines.append(json.loads(line))
 
-        r = compare(dl, dr)
-        print(" Result:", r)
+        # Add our markers
+        lines.append([[2]])
+        lines.append([[6]])
 
-        if r == -1:
-            result += i+1
+        # Some good old python library magic
+        lines = sorted(lines, key=cmp_to_key(lambda p1, p2: compare(p1, p2)))
 
-        print("")
+        # Find markers
+        result = 1
+        for i, p in enumerate(lines):
+            if p == [[2]] or p == [[6]]:
+                result *= i+1
 
-    print("Total: ", result)
-    return result
+        return result
+
+    else:
+        with open(file, 'r') as fp:
+            pairs = [[y for y in x.split("\n")]
+                     for x in fp.read().split("\n\n")]
+
+        result = 0
+        for i, pair in enumerate(pairs):
+            left, right = pair
+
+            dl = json.loads(left)
+            dr = json.loads(right)
+
+            r = compare(dl, dr)
+            if r == -1:
+                result += i+1
+
+        return result
 
 
 # Part 1
 assert parse_file('test.txt') == 13
 print("Part 1: ", parse_file('input.txt'))
 
-# assert parse_file('test.txt', True) == 140
-# print("Part 2: ", parse_file('input.txt', True))
+assert parse_file('test.txt', True) == 140
+print("Part 2: ", parse_file('input.txt', True))
