e68a0146317840f3e0c29538edf68921f7a3f4e6
Karel Bemelmans <karel@narfum.org>
2022-12-20 22:55:26 +0100

    Added 2022 day 17 complete, with inspiration

--- 2022/17/17.py
+++ 2022/17/17.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
+def show(R):
+    max_r = max([r for (r, c) in R])
+    for r in range(max_r, 0, -1):
+        row = ''
+        for c in range(7):
+            if (r, c) in R:
+                row += '#'
+            else:
+                row += ' '
+        print(row)
+
 
 def print_grid(G, B):
     C = 7
@@ -17,133 +28,112 @@ def print_grid(G, B):
         print("")
 
 
-def move_block(block, dir, G):
-    new = set()
+def load_piece(n, r):
+    match n:
+        case 0:
+            return set([(r, 2), (r, 3), (r, 4), (r, 5)])
+        case 1:
+            return set([(r+2, 3), (r+1, 2), (r+1, 3), (r+1, 4), (r, 3)])
+        case 2:
+            return set([(r, 2), (r, 3), (r, 4), (r+1, 4), (r+2, 4)])
+        case 3:
+            return set([(r, 2), (r+1, 2), (r+2, 2), (r+3, 2)])
+        case 4:
+            return set([(r+1, 2), (r, 2), (r+1, 3), (r, 3)])
+        case _:
+            assert False
 
+
+def move_piece(piece, dir):
     match dir:
         case 'left':
-            for (r, c) in block:
-                cc = c - 1
-                if cc < 0:
-                    return False
-                else:
-                    new.add((r, cc))
+            if any([c == 0 for (r, c) in piece]):
+                return piece
+            return set([(r, c-1) for (r, c) in piece])
 
         case 'right':
-            for (r, c) in block:
-                cc = c + 1
-                if cc > 6:
-                    return False
-                else:
-                    new.add((r, cc))
+            if any([c == 6 for (r, c) in piece]):
+                return piece
+            return set([(r, c+1) for (r, c) in piece])
 
         case 'down':
-            for (r, c) in block:
-                rr = r - 1
-                new.add((rr, c))
+            return set([(r-1, c) for (r, c) in piece])
 
-    # Is this a valid move?
-    for (r, c) in new:
-        if (r, c) in G:
-            return False
-
-    return new
+        case 'up':
+            return set([(r+1, c) for (r, c) in piece])
 
 
-def parse_moves(moves, N):
+def signature(R):
+    max_r = max([r for (r, c) in R])
+    return frozenset([(max_r-r, c) for (r, c) in R if max_r-r <= 30])
 
-    # row, columm
-    SHAPES = [
-        [(0, 0), (0, 1), (0, 2), (0, 3)],          # horizontal bar
-        [(0, 1), (1, 0), (1, 1), (1, 2), (2, 1)],  # plus sign
-        [(0, 0), (0, 1), (0, 2), (1, 2), (2, 2)],  # reverse L shape
-        [(0, 0), (1, 0), (2, 0), (3, 0)],          # vertical bar
-        [(0, 0), (1, 0), (0, 1), (1, 1)]           # cube
-    ]
 
-    B = set([])
-    F = 0  # row number of the floor or the highest item
-    G = 0  # the new relative floor level
-    R = 0  # number of the rock we are currently processing
-    new_shape = True
-    buffer = set()
-    m = 0
+def parse_moves(moves, L, p2=False):
 
-    # The tall, vertical chamber is exactly seven units wide. Each rock appears so that its left edge is two units away
-    # from the left wall and its bottom edge is three units above the highest rock in the room (or the floor, if there isn't one).
+    R = set([(0, c) for c in range(7)])
+    SEEN = {}
+    top = 0
+    i = 0
+    t = 0
+    added = 0
 
     # Nr. of rocks that have gotten stable
-    n = 1
-    while n <= N:
-        # Do we need to load a new shape?
-        if new_shape:
-            new_shape = False
+    while t < L:
+        piece = load_piece(t % 5, top+4)
 
-            # We start 2 from the left, 3 from the floor F
-            buffer = set()
-            for (r, c) in SHAPES[R]:
-                rr = r + F + 4
-                cc = c + 2
-                buffer.add((rr, cc))
+        while True:
+            if moves[i] == '<':
+                piece = move_piece(piece, 'left')
+                if piece & R:
+                    piece = move_piece(piece, 'right')
 
-            # Load the next shape
-            R = (R + 1) % 5
+            else:
+                piece = move_piece(piece, 'right')
+                if piece & R:
+                    piece = move_piece(piece, 'left')
 
-        # Otherwise we move in the direction requested, if possible
-        else:
-            move = moves[m % len(moves)]
-            m += 1
+            # Load next move
+            i = (i+1) % len(moves)
 
-            # Calculate our next direction
-            match move:
-                case '<':
-                    next_block = move_block(buffer, "left", B)
-                    if next_block:
-                        buffer = next_block
+            piece = move_piece(piece, 'down')
+            if piece & R:
+                piece = move_piece(piece, 'up')
 
-                case '>':
-                    next_block = move_block(buffer, "right", B)
-                    if next_block:
-                        buffer = next_block
+                # Add this piece to our list of static blocks
+                R |= piece
+                top = max([r for (r, c) in R])
 
-            # Can we still move down?
-            # - we are not at the bottom with this move
-            # - we are not hitting a block that is stable
-            move_down = True
-            finished = False
+                SR = (i, t % 5, signature(R))
+                if SR in SEEN and t >= 2022:
+                    (old_t, old_r) = SEEN[SR]
+                    dr = top-old_r
+                    dt = t-old_t
+                    amt = (L-t)//dt
+                    added += amt*dr
+                    t += amt*dt
+                SEEN[SR] = (t, top)
+                # show(R)
+                break
 
-            next_block = move_block(buffer, "down", B)
-            if not next_block:
-                move_down = False
-                finished = True
+        t += 1
+        if not p2 and t == 2022:
+            return top
 
-            if move_down:
-                buffer = next_block
-                # We hit the ground floor
-                if min([r for r, c in buffer]) == G:
-                    finished = True
-
-            if finished:
-                B.update(buffer)
-                new_shape = True
-                F = max(r for r, c in B)
-                n += 1
-
-    return max(r for r, c in B) + 1
+    return top+added
 
 
-def parse_file(file, N):
+def parse_file(file, N, p2=False):
 
     # Shape is a list of lists that contains pairs
     with open(file, 'r') as fp:
         moves = [char for char in fp.read().strip()]
 
-    return parse_moves(moves, N)
+    return parse_moves(moves, N, p2)
 
 
 # Part 1
 assert parse_file('test.txt', 2022) == 3068
 print("Part 1: ", parse_file('input.txt', 2022))
 
-# assert parse_file('test.txt', int(1e12)) == 1514285714288
-# print("Part 2: ", parse_file('input.txt', 4000000, True))
+assert parse_file('test.txt', int(1e12), True) == 1514285714288
+print("Part 2: ", parse_file('input.txt', 4000000, True))
