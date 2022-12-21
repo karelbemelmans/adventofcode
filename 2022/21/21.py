c641c26703c7d4e1fbc45de46edba75b81ffa534
Karel Bemelmans <karel@narfum.org>
2022-12-21 18:27:32 +0100

    Added 2022 day 21 solution and inspiration

--- 2022/21/21.py
+++ 2022/21/21.py
@@ -4,38 +4,70 @@ from collections import defaultdict
 
 
 def parse(lines, p2=False):
-    R = defaultdict(int)
-    operations = ['+', '-', '*', '/']
+
+    low = 0
+    high = 10000000000000
 
     while True:
-        for k, v in lines:
+        j = (low + high) // 2
+        print(low, high, j)
 
-            # Is it a value? Then store it
-            if any(v.count(o) for o in operations):
-                a, op, b = v.split(" ")
-                # Do we know a and b's value already?
-                if R[a] and R[b]:
-                    if op == '+':
-                        R[k] = R[a] + R[b]
-                    elif op == '-':
-                        R[k] = R[a] - R[b]
-                    elif op == '*':
-                        R[k] = R[a] * R[b]
-                    elif op == '/':
-                        R[k] = R[a] // R[b]
-            else:
-                R[k] = int(v)
+        R = defaultdict(int)
+        R['humn'] = j
 
-        if R['root']:
-            return R['root']
+        left = ""
+        right = ""
+
+        i = 0
+        while i < len(lines):
+            i += 1
+            for k, v in lines:
+
+                if k == 'root':
+                    left, _, right = v.split(" ")
+
+                # p2 ignores humn
+                if p2 and k == 'humn':
+                    continue
+
+                # Operation
+                if any(v.count(o) for o in ['+', '-', '*', '/']):
+                    a, op, b = v.split(" ")
+
+                    if R[a] and R[b]:
+                        if op == '+':
+                            R[k] = R[a] + R[b]
+                        elif op == '-':
+                            R[k] = R[a] - R[b]
+                        elif op == '*':
+                            R[k] = R[a] * R[b]
+                        elif op == '/':
+                            R[k] = R[a] // R[b]
+
+                # A literal value
+                else:
+                    R[k] = int(v)
+
+            if not p2 and R['root']:
+                return R['root']
+
+        diff = R[left] - R[right]
+        # print("diff", left, right, R[left], R[right], diff)
+        if p2 and diff == 0:
+            print("p2 result found:", R['humn'])
+            return R['humn']
+        elif diff < 0:
+            low = j + 1
+        else:
+            high = j - 1
 
 
 def parse_file(file, p2=False):
-
-    # Shape is a list of lists that contains pairs
     with open(file, 'r') as fp:
-        lines = [[parts.strip() for parts in line.split(
-            ":")] for line in fp.readlines()]
+        lines = [
+            [parts.strip() for parts in line.split(":")]
+            for line in fp.readlines()
+        ]
 
     return parse(lines, p2)
 
@@ -44,5 +76,5 @@ def parse_file(file, p2=False):
 assert parse_file('test.txt') == 152
 print("Part 1: ", parse_file('input.txt'))
 
-# assert parse_file('test.txt', int(1e12), True) == 1514285714288
-# print("Part 2: ", parse_file('input.txt', int(1e12), True))
+assert parse_file('test.txt', True) == 301
+# print("Part 2: ", parse_file('input.txt', True))
