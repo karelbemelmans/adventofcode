087178feaa9950a975ed4b3277f38b1c50ab5dd1
Karel Bemelmans <karel@narfum.org>
2022-12-28 19:08:28 +0100

    Simplified 2015 day 6

--- 2015/06/06.py
+++ 2015/06/06.py
@@ -4,56 +4,7 @@ from collections import deque, defaultdict
 from functools import reduce
 
 
-def parse_p1(lines):
-
-    G = set()
-
-    for line in lines:
-        words = line.split(" ")
-
-        if words[0] == 'turn':
-            m = words[1]
-            r1, c1 = words[2].split(",")
-            r2, c2 = words[4].split(",")
-
-        elif words[0] == 'toggle':
-            m = words[0]
-            r1, c1 = words[1].split(",")
-            r2, c2 = words[3].split(",")
-
-        r1 = int(r1)
-        c1 = int(c1)
-        r2 = int(r2)
-        c2 = int(c2)
-
-        assert r2 >= r1
-        assert c2 >= c1
-
-        for r in range(r1, r2+1):
-            for c in range(c1, c2+1):
-
-                # Turn on if not on already
-                if m == 'on':
-                    if not (r, c) in G:
-                        G.add((r, c))
-
-                # Turn off if it was on
-                elif m == 'off':
-                    if (r, c) in G:
-                        G.remove((r, c))
-
-                # Toggle
-                else:
-                    if (r, c) in G:
-                        G.remove((r, c))
-                    else:
-                        G.add((r, c))
-
-    # Return the number of lights we have on
-    return len(G)
-
-
-def parse_p2(lines):
+def parse(lines, p2=False):
 
     G = defaultdict(int)
 
@@ -83,15 +34,24 @@ def parse_p2(lines):
 
                 # Turn on if not on already
                 if m == 'on':
-                    G[(r, c)] += 1
+                    if p2:
+                        G[(r, c)] += 1
+                    else:
+                        G[(r, c)] = 1
 
                 # Turn off if it was on
                 elif m == 'off':
-                    G[(r, c)] = max(0, G[(r, c)]-1)
+                    if p2:
+                        G[(r, c)] = max(0, G[(r, c)]-1)
+                    else:
+                        G[(r, c)] = 0
 
                 # Toggle
                 else:
-                    G[(r, c)] += 2
+                    if p2:
+                        G[(r, c)] += 2
+                    else:
+                        G[(r, c)] = (G[(r, c)] + 1) % 2
 
     # Return the number of lights we have on
     r = reduce(lambda a, b: a+b, G.values())
@@ -103,10 +63,7 @@ def parse_file(file, p2=False):
     with open(file, 'r') as fp:
         lines = [line for line in fp.read().splitlines()]
 
-    if p2:
-        return parse_p2(lines)
-    else:
-        return parse_p1(lines)
+    return parse(lines, p2)
 
 
 # Part 1
