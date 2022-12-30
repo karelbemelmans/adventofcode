c092db2c8ecd5bb9cb04dae0d56f35a8a17c7337
Karel Bemelmans <karel@narfum.org>
2022-12-30 11:19:03 +0100

    Updated template

--- template.py
+++ template.py
@@ -5,7 +5,6 @@ from collections import deque, defaultdict
 
 def parse_file(file, p2=False):
 
-    # Shape is a list of lists that contains pairs
     with open(file, 'r') as fp:
         lines = [line for line in fp.read().splitlines()]
 
@@ -14,5 +13,6 @@ def parse_file(file, p2=False):
 assert parse_file('test.txt') == x
 print("Part 1: ", parse_file('input.txt'))
 
+# Part 2
 # assert parse_file('test.txt', True) == 58
 # print("Part 2: ", parse_file('input.txt', True))
