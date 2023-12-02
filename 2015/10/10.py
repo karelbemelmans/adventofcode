616456a57461213cef35ceb3356c9b3ae385a7ad
Karel Bemelmans <karel@narfum.org>
2023-12-02 16:49:45 +0100

    Added 2015 day 10 part 1

---
+++ 2015/10/10.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+from collections import deque, defaultdict
+
+
+def look_and_say_string(string):
+    R = []
+    current = None
+    count = 0
+
+    for char in string:
+
+        # We are still in the same char? Add 1 to counter
+        if current:
+            if char == current:
+                count += 1
+
+            else:
+                R.append(str(count))
+                R.append(current)
+                current = char
+                count = 1
+
+        else:
+            current = char
+            count = 1
+
+    # Last step is adding the current
+    R.append(str(count))
+    R.append(current)
+
+    return R
+
+
+def parse_string(string, times=40, p2=False):
+
+    for i in range(times):
+        string = look_and_say_string(string)
+
+    return len(string)
+
+
+# Part 1
+assert parse_string('1', 1) == 2
+assert parse_string('11', 1) == 2
+assert parse_string('21', 1) == 4
+assert parse_string('1211', 1) == 6
+assert parse_string('111211', 1) == 6
+assert parse_string('1', 4) == 6
+print("Part 1: ", parse_string('3113322113', 40))
+
+# Part 2
+# assert parse_string('test.txt', True) == 0
+# print("Part 2: ", parse_string('input.txt', True))
