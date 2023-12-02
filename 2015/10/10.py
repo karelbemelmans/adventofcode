9b771d7086af35b628aeecdd8e597ebffe76d837
Karel Bemelmans <karel@narfum.org>
2023-12-02 20:14:27 +0100

    Rewrite 2015 day 10

--- 2015/10/10.py
+++ 2015/10/10.py
@@ -1,42 +1,18 @@
 #!/usr/bin/env python3
 
-from collections import deque, defaultdict
+from itertools import groupby
 
 
 def look_and_say_string(string):
-    R = []
-    current = None
-    count = 0
-
-    for char in string:
-
-        # We are still in the same char? Add 1 to counter
-        if current:
-            if char == current:
-                count += 1
-
-            else:
-                R.append(str(count))
-                R.append(current)
-                current = char
-                count = 1
-
-        else:
-            current = char
-            count = 1
-
-    # Last step is adding the current
-    R.append(str(count))
-    R.append(current)
-
-    return R
+    s = ""
+    for k, g in groupby(string):
+        s += str(len(list(g))) + str(k)
+    return s
 
 
 def parse_string(string, times=40, p2=False):
-
-    for i in range(times):
+    for _ in range(times):
         string = look_and_say_string(string)
-
     return len(string)
 
 
@@ -50,4 +26,4 @@ assert parse_string('1', 4) == 6
 print("Part 1: ", parse_string('3113322113', 40))
 
 # Part 2
-print("Part 1: ", parse_string('3113322113', 50))
+print("Part 2: ", parse_string('3113322113', 50))
