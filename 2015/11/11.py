a9dde35a6bda0011db1773cfa083d70d0b5f54a4
Karel Bemelmans <karel@narfum.org>
2023-12-03 15:09:08 +0100

    Added 2023 day 11 part 1

---
+++ 2015/11/11.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+from collections import deque, defaultdict
+
+
+def next_password(string):
+    '''
+    Incrementing is just like counting with numbers: xx, xy, xz, ya, yb, and so on. 
+    Increase the rightmost letter one step; if it was z, it wraps around to a, and 
+    repeat with the next letter to the left until one doesn't wrap around.
+    '''
+
+    # Do we need to cover this case where the password gets longer?
+    if string == "":
+        return "a"
+
+    start = ord('a')
+    end = ord('z')
+
+    x = string[-1]
+    new = ord(x) + 1
+
+    if new > end:
+        return next_password(string[0:-1]) + chr(start)
+    else:
+        return string[0:-1] + chr(new)
+
+
+def meets_requirements(string):
+    '''
+    Passwords must include one increasing straight of at least three letters, 
+    like abc, bcd, cde, and so on, up to xyz. They cannot skip letters; abd doesn't count.
+    '''
+
+    p1 = False
+    p2 = False
+    p3 = 0
+
+    start = ord('a')
+    end = ord('z')
+
+    for x in range(start, end-1):
+        match = chr(x) + chr(x+1) + chr(x+2)
+        if match in string:
+            p1 = True
+            break
+
+    '''
+    Passwords may not contain the letters i, o, or l, as these letters can be 
+    mistaken for other characters and are therefore confusing.
+    '''
+    banned = set(['i', 'o', 'l'])
+    if not any(x in banned for x in string):
+        p2 = True
+
+    '''
+    Passwords must contain at least two different, non-overlapping pairs of letters, like aa, bb, or zz.
+    '''
+    for x in range(start, end+1):
+        if chr(x)*2 in string:
+            p3 += 1
+
+    return p1 and p2 and p3 >= 2
+
+
+def parse_string(string, p2=False):
+
+    while True:
+        string = next_password(string)
+        if meets_requirements(string):
+            return string
+
+
+# Part 1
+assert parse_string('abcdefgh') == "abcdffaa"
+# assert parse_string('ghijklmn') == "ghjaabcc"
+print("Part 1: ", parse_string('hxbxwxba'))
+
+# Part 2
+# assert parse_string('test.txt', True) == 0
+# print("Part 2: ", parse_string('input.txt', True))
