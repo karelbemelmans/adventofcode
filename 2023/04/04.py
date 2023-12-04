2fc78c1a37c68abc58a57185812f128dd0b6eeae
Karel Bemelmans <karel.bemelmans@volvocars.com>
2023-12-04 16:50:55 +0100

    Added 2023 day 4 both parts

--- 2023/04/04.py
+++ 2023/04/04.py
@@ -11,39 +11,37 @@ def score(i):
         return 2 * score(i-1)
 
 
-def process_cards(cards, p2=False):
-    S = 0
-
-    # Process cards
-    for id in cards.keys():
-        data = cards[id]
-        winners, numbers = data.split(' | ')
-
-        W = set([int(x) for x in winners.split()])
-        N = set([int(x) for x in numbers.split()])
-
-        matches = len(N.intersection(W))
-        S += score(matches)
-
-    return S
-
-
 def parse_file(file, p2=False):
 
     with open(file, 'r') as fp:
         lines = [line for line in fp.read().splitlines()]
 
-    C = {}
+    S = 0
 
-    # Parse cards
-    for line in lines:
-        card, data = line.split(': ')
-        _, id = card.split()
-        C[int(id)] = data
+    # Amount of times we've processed a card
+    T = defaultdict(int)
 
     # Process cards
-    S = process_cards(C, p2)
-    return S
+    for i, line in enumerate(lines):
+
+        T[i] += 1
+
+        card, data = line.split(': ')
+        winners, numbers = data.split(' | ')
+
+        W = [int(x) for x in winners.split()]
+        N = [int(x) for x in numbers.split()]
+
+        matches = len(set(W) & set(N))
+        S += score(matches)
+
+        for j in range(matches):
+            T[i + 1 + j] += T[i]
+
+    if p2:
+        return sum(T.values())
+    else:
+        return S
 
 
 # Part 1
@@ -51,5 +49,5 @@ assert parse_file('test.txt') == 13
 print("Part 1: ", parse_file('input.txt'))
 
 # Part 2
-# assert parse_file('test.txt', True) == 30
-# print("Part 2: ", parse_file('input.txt', True))
+assert parse_file('test.txt', True) == 30
+print("Part 2: ", parse_file('input.txt', True))
