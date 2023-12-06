b25ba16dea39697638ba7fa4919b68bf604507a2
Karel Bemelmans <karel.bemelmans@volvocars.com>
2023-12-06 09:16:28 +0100

    Added 2023 day 6 part 2

--- 2023/06/06.py
+++ 2023/06/06.py
@@ -7,8 +7,13 @@ from functools import reduce
 def parse_file(file, p2=False):
 
     times, distances = open(file).read().splitlines()
+
     times = list(map(int, times.split(":")[1].split()))
     distances = list(map(int, distances.split(":")[1].split()))
+
+    if p2:
+        times = [int(reduce(lambda a, b: str(a)+str(b), times))]
+        distances = [int(reduce(lambda a, b: str(a)+str(b), distances))]
 
     winners = defaultdict(int)
     for i in range(len(times)):
@@ -28,5 +33,5 @@ assert parse_file('test.txt') == 288
 print("Part 1: ", parse_file('input.txt'))
 
 # Part 2
-# assert parse_file('test.txt', True) == 0
-# print("Part 2: ", parse_file('input.txt', True))
+assert parse_file('test.txt', True) == 71503
+print("Part 2: ", parse_file('input.txt', True))
