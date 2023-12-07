be71b6eb5cc8b16677c298dc86ad7afd9ebf5adf
Karel Bemelmans <karel@narfum.org>
2023-12-07 10:19:05 +0100

    Added 2023 day 7 both parts

--- 2023/07/07.py
+++ 2023/07/07.py
@@ -1,39 +1,47 @@
 #!/usr/bin/env python3
 
-from collections import deque, defaultdict, Counter
-from functools import cmp_to_key, reduce
+from collections import Counter
+from functools import cmp_to_key
 
 CARDS = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+CARDS_P2 = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J']
 
 
-def hand_strength(hand):
+def hand_strength(hand, p2=False):
     c = Counter(hand)
-    c = sorted(c.values(), reverse=True)
 
-    if c == [5]:
-        return 7
+    # If we have 5 jokers, make sure we process it as five the same cards
+    counts = (
+        [0] if (jokers := c.pop("J", 0)
+                if p2 else 0) == 5 else sorted(c.values())
+    )
+    counts[-1] += jokers
 
-    if c == [4, 1]:
-        return 6
+    match counts:
+        case *_, 5:
+            return 7
 
-    if c == [3, 2]:
-        return 5
+        case *_, 4:
+            return 6
 
-    if c == [3, 1, 1]:
-        return 4
+        case *_, 2, 3:
+            return 5
 
-    if c == [2, 2, 1]:
-        return 3
+        case *_, 3:
+            return 4
 
-    if c == [2, 1, 1, 1]:
-        return 2
+        case *_, 2, 2:
+            return 3
+
+        case *_, 2:
+            return 2
 
     return 1
 
 
-def compare_hands(x, y):
-    score_x = hand_strength(x[0])
-    score_y = hand_strength(y[0])
+def compare_hands(x, y, p2=False, cards=CARDS):
+    score_x = hand_strength(x[0], p2)
+    score_y = hand_strength(y[0], p2)
 
     if score_x > score_y:
         return 1
@@ -41,12 +49,16 @@ def compare_hands(x, y):
         return -1
     else:
         for (xx, yy) in zip(x[0], y[0]):
-            if CARDS.index(xx) < CARDS.index(yy):
+            if cards.index(xx) < cards.index(yy):
                 return 1
-            if CARDS.index(xx) > CARDS.index(yy):
+            if cards.index(xx) > cards.index(yy):
                 return -1
         else:
             return 0
+
+
+def compare_hands_p2(x, y):
+    return compare_hands(x, y, True, CARDS_P2)
 
 
 def parse_file(file, p2=False):
@@ -54,12 +66,18 @@ def parse_file(file, p2=False):
     with open(file, 'r') as fp:
         lines = [line for line in fp.read().splitlines()]
 
+    # Build our input as tuples of (cards, score)
     hands = []
     for i, line in enumerate(lines):
         hands.append((line.split(' ')[0], int(line.split(' ')[1])))
 
-    hands = sorted(hands, key=cmp_to_key(compare_hands))
+    # Sort our hands depending which phase we are (can this be done nicer?)
+    if p2:
+        hands = sorted(hands, key=cmp_to_key(compare_hands_p2))
+    else:
+        hands = sorted(hands, key=cmp_to_key(compare_hands))
 
+    # Calculate our score
     S = 0
     for i, hand in enumerate(hands):
         score = hand[1] * (i+1)
@@ -73,5 +91,5 @@ assert parse_file('test.txt') == 6440
 print("Part 1: ", parse_file('input.txt'))
 
 # Part 2
-# assert parse_file('test.txt', True) == 0
-# print("Part 2: ", parse_file('input.txt', True))
+assert parse_file('test.txt', True) == 5905
+print("Part 2: ", parse_file('input.txt', True))
