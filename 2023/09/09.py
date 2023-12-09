73acad4c8a3462541ef031ad9e77675873fec694
Karel Bemelmans <karel@narfum.org>
2023-12-09 16:02:52 +0100

    Added 2023 day 9 part 1

--- 2023/09/09.py
+++ 2023/09/09.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-from collections import deque, defaultdict
-
 
 def parse_file(file, p2=False):
 
@@ -9,20 +7,49 @@ def parse_file(file, p2=False):
         lines = [line for line in fp.read().splitlines()]
 
     S = 0
-    for line in lines:
-
+    for nr, line in enumerate(lines):
         values = [int(x) for x in line.split(' ')]
-        print(values)
+        L = [values]
 
-        for i in range(len(values)):
-            d = abs(values[i+1] - values[i])
-            print(i, d)
+        done = False
+        while not done:
+            new = []
+
+            for i in range(len(values)-1):
+                diff = values[i+1] - values[i]
+                new.append(diff)
+
+            values = new
+            L.append(values)
+
+            # Are we done?
+            if sum(values) == 0:
+                done = True
+
+        # At this point we have L with a list of all the differences up to 0
+        # Now work the other way around and count up
+        for l in L:
+            print(l)
+
+        # Add a zero to our last list
+        L[-1].append(0)
+        L.reverse()
+
+        for i in range(len(L)-1):
+            new = L[i+1][-1] + L[i][-1]
+            L[i+1].append(new)
+
+        s = L[-1][-1]
+        print("score: ", s)
+        S += s
 
     return S
 
 
 # Part 1
 assert parse_file('test.txt') == 114
+assert parse_file('test2.txt') == (-12 - 93)
+assert parse_file('test3.txt') == -9295
 print("Part 1: ", parse_file('input.txt'))
 
 # Part 2
