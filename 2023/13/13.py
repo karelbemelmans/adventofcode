6f5e595dd545d7cc3ecc86a679127d20813b8e27
Karel Bemelmans <karel@narfum.org>
2023-12-13 08:57:14 +0100

    Added 2023 day 13 both parts

--- 2023/13/13.py
+++ 2023/13/13.py
@@ -3,56 +3,45 @@
 from collections import deque, defaultdict
 
 
-def find_reflections(grid):
-    print(grid)
+def find_reflections(grid, p2=False):
 
     R = len(grid)
     C = len(grid[0])
-    print(R, C)
 
     # Columns
-    print("Checking columns")
     for c in range(1, C):
-
-        # Length of the reflection, decided by the column
         l = min(c, C-c)
-        print("Column", c, l)
 
-        mirror = True
+        errors = 0
         for r in range(R):
-            print("Row: ", r)
-
             for i in range(0, l):
                 cl = c-1-i
                 cr = c+i
-                print(r, cl, r, cr, grid[r][cl], grid[r][cr])
                 if not grid[r][cl] == grid[r][cr]:
-                    mirror = False
-                    break
+                    errors += 1
 
-        if mirror:
+        if p2:
+            if errors == 1:
+                return c
+        elif errors == 0:
             return c
 
     # Row
-    print("Checking rows")
     for r in range(1, R):
-
-        # Length of the reflection, decided by the column
         l = min(r, R-r)
-        print("Row", r, l)
 
-        mirror = True
+        errors = 0
         for c in range(C):
-            print("Column: ", c)
-
             for i in range(0, l):
                 rl = r-1-i
                 rr = r+i
                 if not grid[rl][c] == grid[rr][c]:
-                    mirror = False
-                    break
+                    errors += 1
 
-        if mirror:
+        if p2:
+            if errors == 1:
+                return r * 100
+        elif errors == 0:
             return r * 100
 
     return 0
@@ -69,12 +58,8 @@ def parse_file(file, p2=False):
         grid = [[char for char in line] for line in block.splitlines()]
 
         # Find reflected columns
-        T += find_reflections(grid)
+        T += find_reflections(grid, p2)
 
-    # To summarize your pattern notes, add up the number of columns to the left
-    # of each vertical line of reflection; to that, also add 100 multiplied by
-    # the number of rows above each horizontal line of reflection.
-    print("Score: ", T)
     return T
 
 
@@ -85,5 +70,7 @@ assert parse_file('test3.txt') == 405
 print("Part 1: ", parse_file('input.txt'))
 
 # Part 2
-# assert parse_file('test.txt', True) == 0
-# print("Part 2: ", parse_file('input.txt', True))
+assert parse_file('test.txt', True) == 300
+assert parse_file('test2.txt', True) == 100
+assert parse_file('test3.txt', True) == 400
+print("Part 2: ", parse_file('input.txt', True))
