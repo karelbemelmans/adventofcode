3de34669dacbfafa4806ea6108d05a4a20930199
Karel Bemelmans <karel@narfum.org>
2023-12-13 15:37:31 +0100

    Added comments

--- 2023/13/13.py
+++ 2023/13/13.py
@@ -5,22 +5,38 @@ def find_reflections(grid, p2=False):
     R = len(grid)
     C = len(grid[0])
 
-    # Columns
+    # We look for mirrors in the vertical side first, so loop over columns
+    # Our "pointer" is called c and means the place between column c-1 and c
+    # So we can start our range at 1, skipping the first column, and go on to the last one
     for c in range(1, C):
+
+        # The discrepancy between our column and the edge of the grid can be at most l
         l = min(c, C-c)
 
+        # We keep a counter for all the errors we get, meaning the times a char
+        # at position r,c is not the same as the one at r,C-c
         errors = 0
+
+        # We scan per row, from top to bottom, going over all rows
         for r in range(R):
+
+            # We check the left and right side of the column, with an increasing
+            # offset that goes from 0 to l
             for i in range(l):
+
+                # The coords of the mirror character
                 cl = c-1-i
                 cr = c+i
+
+                # If it's not the same, we add an error and continue anyway
                 if not grid[r][cl] == grid[r][cr]:
                     errors += 1
 
+        # The result is now the column that has either 0 or 1 errors
         if errors == (1 if p2 else 0):
             return c
 
-    # Row
+    # Rows. Same as above, but we loop over rows and scan columns
     for r in range(1, R):
         l = min(r, R-r)
 
