932db4de818e940913cddb1efe8b44a21f8de6f4
Karel Bemelmans <karel@narfum.org>
2023-12-15 08:41:28 +0100

    Added 2023 day 15 both parts

--- 2023/15/15.py
+++ 2023/15/15.py
@@ -19,13 +19,52 @@ def parse_file(file, p2=False):
     with open(file, 'r') as fp:
         pieces = [line for line in fp.read().splitlines()][0].split(",")
 
-    cur = 0
+    T = 0
+
+    # Let's get p1 out of the way
+    if not p2:
+        for p in pieces:
+            T += do_hash(p)
+        return T
+
+    # p2
+    BOXES = defaultdict(list)
 
     for p in pieces:
-        cur += do_hash(p)
 
-    print("Score:", cur)
-    return cur
+        if p.count("="):
+            box, val = p.split("=")
+            val = int(val)
+            cur = do_hash(box)
+
+            # If there is already a lens in the box with the same label, replace the old lens with the new lens:
+            # remove the old lens and put the new lens in its place, not moving any other lenses in the box.
+            for i in range(0, 10):
+                if (box, i) in BOXES[cur]:
+                    BOXES[cur] = [x if x != (box, i) else (
+                        box, val) for x in BOXES[cur]]
+                    break
+
+            # If there is not already a lens in the box with the same label, add the lens to the box immediately behind
+            # any lenses already in the box. Don't move any of the other lenses when you do this. If there aren't any
+            # lenses in the box, the new lens goes all the way to the front of the box.
+            else:
+                BOXES[cur].append((box, val))
+
+        elif p.count("-"):
+            box, _ = p.split("-")
+            cur = do_hash(box)
+            for i in range(0, 10):
+                if (box, i) in BOXES[cur]:
+                    BOXES[cur].remove((box, i))
+
+    # Calculate p2 score
+    for box in BOXES:
+        for i, slot in enumerate(BOXES[box]):
+            score = (box+1) * (i+1) * slot[1]
+            T += score
+
+    return T
 
 
 # Part 1
@@ -34,5 +73,5 @@ assert parse_file('test.txt') == 1320
 print("Part 1: ", parse_file('input.txt'))
 
 # Part 2
-# assert parse_file('test.txt', True) == 0
-# print("Part 2: ", parse_file('input.txt', True))
+assert parse_file('test.txt', True) == 145
+print("Part 2: ", parse_file('input.txt', True))
