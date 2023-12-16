42634610be20d3c4c8653e2cd944ea267149391d
Karel Bemelmans <karel@narfum.org>
2023-12-16 11:27:20 +0100

    WIP

--- 2023/16/16.py
+++ 2023/16/16.py
@@ -34,92 +34,96 @@ def parse_file(file, p2=False):
     R = len(G)
     C = len(G[0])
 
-    # List of beams. Every beam has a coordinate and a direction
-    # We start with a beam at 0,0 pointing East
-    B = deque([(0, 0, 'E')])
+    T = 0
+    start = [(0, 0, 'E')]
 
-    # Set of points that have been energized
-    E = set()
+    for S in start:
 
-    i = 0
-    while True:
-        i += 1
+        # List of beams. Every beam has a coordinate and a direction
+        # We start with a beam at 0,0 pointing East
+        B = deque([S])
 
-        # No more beams to process?
-        if len(B) == 0:
-            break
+        # Set of points that have been energized
+        E = set()
 
-        # Get the current beam
-        r, c, d = B.popleft()
+        while True:
 
-        # If we have been here before, we end this beam
-        # The direction is important, because we can have two beams pass through
-        # the same point but with different directions!
-        if (r, c, d) in E:
-            continue
+            # No more beams to process?
+            if len(B) == 0:
+                break
 
-        E.add((r, c, d))
+            # Get the current beam
+            r, c, d = B.popleft()
 
-        # Look at which point we are current at
-        match G[r][c]:
-            case '.':
-                pass
+            # If we have been here before, we end this beam
+            # The direction is important, because we can have two beams pass through
+            # the same point but with different directions!
+            if (r, c, d) in E:
+                continue
 
-            case '/':
-                if d == 'N':
-                    d = 'E'
-                elif d == 'E':
-                    d = 'N'
-                elif d == 'S':
-                    d = 'W'
-                elif d == 'W':
-                    d = 'S'
+            E.add((r, c, d))
 
-            case '\\':
-                if d == 'N':
-                    d = 'W'
-                elif d == 'E':
-                    d = 'S'
-                elif d == 'S':
-                    d = 'E'
-                elif d == 'W':
-                    d = 'N'
+            # Look at which point we are current at
+            match G[r][c]:
+                case '.':
+                    pass
 
-            case '-':
-                if d == 'N' or d == 'S':
-                    # Our beam splits into two beams
-                    B.append((r, c, 'E'))
-                    B.append((r, c, 'W'))
-                    continue
+                case '/':
+                    if d == 'N':
+                        d = 'E'
+                    elif d == 'E':
+                        d = 'N'
+                    elif d == 'S':
+                        d = 'W'
+                    elif d == 'W':
+                        d = 'S'
 
-            case '|':
-                if d == 'E' or d == 'W':
-                    # Our beam splits into two beams
-                    B.append((r, c, 'N'))
-                    B.append((r, c, 'S'))
-                    continue
+                case '\\':
+                    if d == 'N':
+                        d = 'W'
+                    elif d == 'E':
+                        d = 'S'
+                    elif d == 'S':
+                        d = 'E'
+                    elif d == 'W':
+                        d = 'N'
 
-        # print("  - Moving beam", r, c, d)
+                case '-':
+                    if d == 'N' or d == 'S':
+                        # Our beam splits into two beams
+                        B.append((r, c, 'E'))
+                        B.append((r, c, 'W'))
+                        continue
 
-        # Move to the next spot
-        if d == 'N':
-            r -= 1
-        elif d == 'S':
-            r += 1
-        elif d == 'W':
-            c -= 1
-        elif d == 'E':
-            c += 1
+                case '|':
+                    if d == 'E' or d == 'W':
+                        # Our beam splits into two beams
+                        B.append((r, c, 'N'))
+                        B.append((r, c, 'S'))
+                        continue
 
-        # Add the new beam to the list if the spot is valid
-        if 0 <= r < R and 0 <= c < C:
-            B.append((r, c, d))
+            # print("  - Moving beam", r, c, d)
 
-    S = set()
-    for r, c, _ in E:
-        S.add((r, c))
+            # Move to the next spot
+            if d == 'N':
+                r -= 1
+            elif d == 'S':
+                r += 1
+            elif d == 'W':
+                c -= 1
+            elif d == 'E':
+                c += 1
 
-    T = len(S)
+            # Add the new beam to the list if the spot is valid
+            if 0 <= r < R and 0 <= c < C:
+                B.append((r, c, d))
+
+        S = set()
+        for r, c, _ in E:
+            S.add((r, c))
+
+        T = max(T, len(S))
+
     return T
 
 
