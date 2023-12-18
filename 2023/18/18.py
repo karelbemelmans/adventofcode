c170244f7926c7aebc49fe706f627a1cc84a8c95
Karel Bemelmans <karel.bemelmans@volvocars.com>
2023-12-18 11:45:33 +0100

    Rewrite

--- 2023/18/18.py
+++ 2023/18/18.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
 
-from collections import deque, defaultdict
-import random
-
 DIR = {
     'U': (-1, 0),
     'D': (1, 0),
     'L': (0, -1),
     'R': (0, 1)
 }
-
-default_color = "#000000"
 
 
 def show_set(S, p2=False):
@@ -22,20 +17,17 @@ def show_set(S, p2=False):
     for r in range(min_r, max_r+1):
         for c in range(min_c, max_c+1):
             if (r, c) in S:
-                if p2:
-                    print(S[(r, c)], end='')
+                if (r, c) in S:
+                    print('#', end='')
                 else:
-                    if not S[(r, c)] == default_color:
-                        print('#', end='')
-                    else:
-                        print('.', end='')
+                    print('.', end='')
             else:
                 print('.', end='')
         print()
     print()
 
 
-def floodFill(G, r,  c, prevC, newC):
+def floodFill(G, r,  c):
     min_r = min(r for r, _ in G)
     max_r = max(r for r, _ in G)
     min_c = min(c for _, c in G)
@@ -51,7 +43,7 @@ def floodFill(G, r,  c, prevC, newC):
     queue.append((r, c))
 
     # Color the pixel with the new color
-    G[(r, c)] = newC
+    G.add((r, c))
 
     # While the queue is not empty i.e. the whole component having prevC color is not colored with newC color
     while queue:
@@ -61,12 +53,10 @@ def floodFill(G, r,  c, prevC, newC):
 
         # Check if the adjacent pixels are valid
         for d in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-            rr = cur[0] + d[0]
-            cc = cur[1] + d[1]
-            if (rr, cc) in G and not d == (0, 0):
-                if G[(rr, cc)] == prevC:
-                    G[(rr, cc)] = newC
-                    queue.append((rr, cc))
+            rr, cc = cur[0] + d[0], cur[1] + d[1]
+            if not (rr, cc) in G and not d == (0, 0):
+                G.add((rr, cc))
+                queue.append((rr, cc))
 
 
 def parse_file(file, p2=False):
@@ -75,7 +65,7 @@ def parse_file(file, p2=False):
         lines = [[piece for piece in line.split(' ')]
                  for line in fp.read().splitlines()]
 
-    S = {}
+    S = set()
 
     # Build up our set of points
     p = (0, 0)
@@ -83,29 +73,14 @@ def parse_file(file, p2=False):
         d, l, color = DIR[line[0]], int(line[1]), line[2][1:-1]
         for _ in range(1, l+1):
             rr, cc = p[0] + d[0], p[1] + d[1]
-            S[(rr, cc)] = color if p2 else "#"
+            S.add((rr, cc))
             p = (rr, cc)
 
-    # Give all the other points a color as well
-    min_r = min(r for r, _ in S)
-    max_r = max(r for r, _ in S)
-    min_c = min(c for _, c in S)
-    max_c = max(c for _, c in S)
+    floodFill(S, 1, 1)
 
-    for r in range(min_r, max_r+1):
-        for c in range(min_c, max_c+1):
-            if not (r, c) in S:
-                S[(r, c)] = default_color
+    # show_set(S)
 
-    print("Set before closing:")
-    show_set(S, p2)
-
-    floodFill(S, 1, 1, default_color, "#")
-
-    print("Set after closing:")
-    show_set(S, p2)
-
-    T = len([color for color in S.values() if color == "#"])
+    T = len(S)
     print("Score: ", T)
     return T
 
