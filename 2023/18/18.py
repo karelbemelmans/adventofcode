1d7ae9ea4e8bbe7000a3f8306a346017b45f5a53
Karel Bemelmans <karel.bemelmans@volvocars.com>
2023-12-18 14:30:53 +0100

    Added 2023 day 18 both parts

--- 2023/18/18.py
+++ 2023/18/18.py
@@ -8,55 +8,21 @@ DIR = {
 }
 
 
-def show_set(S, p2=False):
-    min_r = min(r for r, _ in S)
-    max_r = max(r for r, _ in S)
-    min_c = min(c for _, c in S)
-    max_c = max(c for _, c in S)
+def segments(p):
+    '''
+    This function is a helper function that turns a list into a list of tuples:
 
-    for r in range(min_r, max_r+1):
-        for c in range(min_c, max_c+1):
-            if (r, c) in S:
-                if (r, c) in S:
-                    print('#', end='')
-                else:
-                    print('.', end='')
-            else:
-                print('.', end='')
-        print()
-    print()
+    Input:   ['a', 'b', 'c', 'd']
+    Returns: [('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'a')]
+    '''
+    return zip(p, p[1:] + [p[0]])
 
 
-def floodFill(G, r,  c):
-    min_r = min(r for r, _ in G)
-    max_r = max(r for r, _ in G)
-    min_c = min(c for _, c in G)
-    max_c = max(c for _, c in G)
-
-    R = max_r - min_r
-    C = max_c - min_c
-
-    queue = []
-
-    # Append the position of starting
-    # pixel of the component
-    queue.append((r, c))
-
-    # Color the pixel with the new color
-    G.add((r, c))
-
-    # While the queue is not empty i.e. the whole component having prevC color is not colored with newC color
-    while queue:
-
-        # Dequeue the front node
-        cur = queue.pop()
-
-        # Check if the adjacent pixels are valid
-        for d in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-            rr, cc = cur[0] + d[0], cur[1] + d[1]
-            if not (rr, cc) in G and not d == (0, 0):
-                G.add((rr, cc))
-                queue.append((rr, cc))
+def poly_area(p):
+    '''
+    This function calculates the area of a polygon given a list of points
+    '''
+    return 0.5 * abs(sum(x0 * y1 - x1 * y0 for ((x0, y0), (x1, y1)) in segments(p)))
 
 
 def parse_file(file, p2=False):
@@ -65,15 +31,17 @@ def parse_file(file, p2=False):
         lines = [[piece for piece in line.split(' ')]
                  for line in fp.read().splitlines()]
 
-    S = set()
-
     # Pointer coords
     p = (0, 0)
 
+    # Corners
+    S = []
+    outline = 0
+
     # Build up our set of points
     for line in lines:
-        d = int(code[-1]) if p2 else line[0]
-        l = int(code[1:-1], 16) if p2 else int(line[1])
+        d = int(line[2][-2:-1]) if p2 else line[0]
+        l = int(line[2][2:-2], 16) if p2 else int(line[1])
 
         # 0 means R, 1 means D, 2 means L, and 3 means U.
         match d:
@@ -87,31 +55,18 @@ def parse_file(file, p2=False):
                 d = DIR['U']
 
         # End point of our line
-        end = (p[0] + l * d[0], p[1] + l * d[1])
+        p = (p[0] + l * d[0], p[1] + l * d[1])
+        outline += l
+        S.append(p)
 
-        # We add every point along the way
-        for i in range(1, l+1):
-            rr, cc = p[0] + (i * d[0]), p[1] + (i * d[1])
-            S.add((rr, cc))
-
-        # Our new pointer location
-        p = (rr, cc)
-
-    show_set(S)
-
-    floodFill(S, 1, 1)
-
-    show_set(S)
-
-    T = len(S)
-    print("Score: ", T)
+    T = int(poly_area(S)) + outline // 2 + 1
     return T
 
 
 # Part 1
 assert parse_file('test.txt') == 62
-# print("Part 1: ", parse_file('input.txt'))
+print("Part 1: ", parse_file('input.txt'))
 
 # Part 2
-# assert parse_file('test.txt', True) == 952408144115
-# print("Part 2: ", parse_file('input.txt', True))
+assert parse_file('test.txt', True) == 952408144115
+print("Part 2: ", parse_file('input.txt', True))
