d090cb45a02871887249d6903838a9b8fbfa2939
Karel Bemelmans <karel.bemelmans@volvocars.com>
2023-12-18 18:55:30 +0100

    Rewrite 2023 day 10 with using the shoelance algoritm

--- 2023/10/10.py
+++ 2023/10/10.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from matplotlib.path import Path
+import sys
 
 # Give our directions a nice name
 UP = (-1, 0)
@@ -15,6 +15,30 @@ DIR = {
     RIGHT: ['-', 'J', '7'],
     DOWN: ['|', 'J', 'L']
 }
+
+
+def poly_area(p):
+    '''
+    This function calculates the area of a polygon given a list of points
+
+    https://en.wikipedia.org/wiki/Shoelace_formula
+    '''
+    return 0.5 * abs(sum(x0 * y1 - x1 * y0 for ((x0, y0), (x1, y1)) in segments(p)))
+
+
+def segments(p):
+    '''
+    This function is a helper function that turns a list into a list of tuples:
+
+    Single value:
+    Input:   ['a', 'b', 'c', 'd']
+    Returns: [('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'a')]
+
+    2D tuples:
+    Input:   [('a', 'a'), ('b', 'b'), ('c', 'c')]
+    Output:  [(('a', 'a'), ('b', 'b')), (('b', 'b'), ('c', 'c')), (('c', 'c'), ('a', 'a'))]
+    '''
+    return zip(p, p[1:] + [p[0]])
 
 
 def connects(x, y, dir):
@@ -68,6 +92,7 @@ def parse_file(file, p2=False):
 
     # Path of all visited nodes in our circular path
     path = [S]
+    outline = 0
 
     done = False
     while not done:
@@ -87,37 +112,36 @@ def parse_file(file, p2=False):
                 if connects(grid[r][c], grid[rr][cc], (dr, dc)) and not (rr, cc) in path:
                     current = (rr, cc)
                     path.append((rr, cc))
+                    outline += 1
                     break
         # If we have no more connections, we are done
         else:
             done = True
 
     # p2: Find isolated points
-    # matplotlib.path makes this ridiculously easy :)
     if p2:
-        T = 0
-        p = Path(path)
-        for r in range(R):
-            for c in range(C):
-                if (r, c) in path:
-                    continue
-                if p.contains_point((r, c)):
-                    T += 1
+        T = int(poly_area(path)) - len(path) // 2 + 1
         return T
 
     # We have a circular path, so use halfway of it as the furthest point
     else:
-        return len(path) // 2
+        T = outline // 2 + 1
+        return T
 
 
-# Part 1
-assert parse_file('test.txt') == 4
-assert parse_file('test-complex.txt') == 4
-assert parse_file('test2.txt') == 8
-assert parse_file('test2-complex.txt') == 8
-print("Part 1: ", parse_file('input.txt'))
+def main():
+    # Part 1
+    assert parse_file('test.txt') == 4
+    assert parse_file('test-complex.txt') == 4
+    assert parse_file('test2.txt') == 8
+    assert parse_file('test2-complex.txt') == 8
+    print("Part 1: ", parse_file('input.txt'))
 
-# Part 2
-assert parse_file('test3.txt', True) == 4
-assert parse_file('test4.txt', True) == 8
-print("Part 2: ", parse_file('input.txt', True))
+    # Part 2
+    assert parse_file('test3.txt', True) == 4
+    assert parse_file('test4.txt', True) == 8
+    print("Part 2: ", parse_file('input.txt', True))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
