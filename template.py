687e4255c2bba1406c9ba777d29f81d1e3ed8a70
Karel Bemelmans <karel.bemelmans@volvocars.com>
2023-12-19 09:10:38 +0100

    Modified template

--- template.py
+++ template.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 from collections import deque, defaultdict
 
 
@@ -7,14 +8,19 @@ def parse_file(file, p2=False):
 
     with open(file, 'r') as fp:
         lines = [line for line in fp.read().splitlines()]
-        
+
     return 0
 
 
-# Part 1
-assert parse_file('test.txt') == 0
-print("Part 1: ", parse_file('input.txt'))
+def main():
+    # Part 1
+    assert parse_file('test.txt') == 0
+    print("Part 1: ", parse_file('input.txt'))
 
-# Part 2
-assert parse_file('test.txt', True) == 0
-print("Part 2: ", parse_file('input.txt', True))
+    # Part 2
+    assert parse_file('test.txt', True) == 0
+    print("Part 2: ", parse_file('input.txt', True))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
