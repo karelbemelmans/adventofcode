0e2684e54576bac9210b624b55dc6d67e8c819da
Karel Bemelmans <karel@narfum.org>
2023-12-22 22:16:30 +0100

    WIP

--- 2023/22/22.py
+++ 2023/22/22.py
@@ -1,8 +1,10 @@
+# %%
 #!/usr/bin/env python3
 
 import sys
 from collections import deque, defaultdict
 from typing import cast
+from functools import reduce
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -14,68 +16,58 @@ def parse_file(file, p2=False):
     with open(file, 'r') as fp:
         lines = [line for line in fp.read().splitlines()]
 
-    # Build 3D grid
+    MAX = 10
 
     # Our bricks
-    B = set()
+    B = defaultdict(int)
+    x, y, z = np.indices((MAX, MAX, MAX))
 
-    for line in lines:
+    # Add our inputs
+    for i, line in enumerate(lines):
         start, end = line.split('~')
-        brick = set()
 
         ax, ay, az = map(int, start.split(','))
         bx, by, bz = map(int, end.split(','))
 
-        for x in range(ax, bx+1) if ax < bx else range(bx, ax+1):
-            for y in range(ay, by+1) if ay < by else range(by, ay+1):
-                for z in range(az, bz+1) if az < bz else range(bz, az+1):
-                    brick.add((x, y, z))
+        (x1, x2) = (ax, bx) if ax < bx else (bx, ax)
+        (y1, y2) = (ay, by) if ay < by else (by, ay)
+        (z1, z2) = (az, bz) if az < bz else (bz, az)
 
-        B.add(frozenset(brick))
-
-    # 3D plot of our input
+        B[i] = (x1 <= x) & (x <= x2) & (y1 <= y) & (y <= y2) & (z1 <= z) & (z <= z2)
 
     # Create axis
-    axes = [10] * 3
-
-    # Data
-    data = np.ones(axes, dtype=bool)
-
-    # Control Transparency
-    alpha = 0.9
+    axes = [MAX] * 3
 
     # Control colour
-    colors = np.empty(axes + [4], dtype=np.float32)
+    colors = np.empty(axes, dtype=object)
+    COLORS = ['red', 'green', 'blue', 'yellow', 'purple', 'orange', 'pink']
+    for i in range(len(B)):
+        colors[B[i]] = COLORS[i % len(COLORS)]
 
-    colors[0] = [1, 0, 0, alpha]  # red
-    colors[1] = [0, 1, 0, alpha]  # green
-    colors[2] = [0, 0, 1, alpha]  # blue
-    colors[3] = [1, 1, 0, alpha]  # yellow
-    colors[4] = [1, 1, 1, alpha]  # grey
+    # Voxels
+    voxelarray = reduce(np.logical_or, B.values())
 
     # Plot figure
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
+    ax.voxels(voxelarray, facecolors=colors, edgecolor='k')
 
-    # Voxels is used to customizations of the sizes, positions and colors.
-    for i, brick in enumerate(B):
-        data = np.zeros(axes, dtype=bool)
-        for x, y, z in brick:
-            data[x, y, z] = True
-        ax.voxels(data, facecolors=colors, edgecolors='grey')
+    plt.show()
 
-    # return 0
+    return 0
 
 
 def main():
     # Part 1
-    assert parse_file('test.txt') == 5
-    print("Part 1: ", parse_file('input.txt'))
+    assert parse_file('test.txt') == 0
+    # print("Part 1: ", parse_file('input.txt'))
 
     # Part 2
-    assert parse_file('test.txt', True) == 0
-    print("Part 2: ", parse_file('input.txt', True))
+    # assert parse_file('test.txt', True) == 0
+    # print("Part 2: ", parse_file('input.txt', True))
 
 
 if __name__ == "__main__":
     sys.exit(main())
+
+# %%
