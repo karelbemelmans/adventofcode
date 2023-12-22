e8da175a6e9b53b9e0f08540716acf6e79bc4522
Karel Bemelmans <karel@narfum.org>
2023-12-22 15:56:21 +0100

    Cleanup

--- 2023/20/20.py
+++ 2023/20/20.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+#
+# Source: https://github.com/mrphlip/aoc/blob/master/2023/20.md
+
+# I wanted to come up with a Python solution that uses a proper OO model to
+# handle this, with objects that maintain a state.
+
+# I ended up with taking the code posted above, adopt it to my structure of AoC
+# and learn from it.
+
 
 import sys
 from collections import deque
--- 2023/20/README.md
+++
@@ -1,7 +0,0 @@
-Source: https://github.com/mrphlip/aoc/blob/master/2023/20.md
-
-I wanted to come up with a Python solution that uses a proper OO model to handle this,
-with objects that maintain a state.
-
-I ended up with taking the code posted above, adopt it to my structure of AoC and
-learn from it.
