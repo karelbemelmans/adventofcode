77650e1e6d51432270a23d9da901882d6f1c90c0
Karel Bemelmans <karel@narfum.org>
2023-12-23 10:30:09 +0100

    Added 2023 day 22 both parts rewrite

--- 2023/22/22.py
+++ 2023/22/22.py
@@ -1,73 +1,50 @@
-# %%
 #!/usr/bin/env python3
+#
+# https://www.reddit.com/r/adventofcode/comments/18o7014/comment/kefvjex/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+#
+# I spent quite a time writing a very long and explicit process
+# This code does the same but in a much much more elegant Python way...
 
 import sys
-from collections import deque, defaultdict
-from typing import cast
-from functools import reduce
-
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 
+def drop(stack, skip=None):
+    peaks = np.zeros((12, 12))
+    falls = 0
+
+    for i, (u, v, w, x, y, z) in enumerate(stack):
+        if i == skip:
+            continue
+
+        peak = peaks[u:x, v:y].max()
+        peaks[u:x, v:y] = peak + z-w
+
+        stack[i] = u, v, peak, x, y, peak + z-w
+        falls += peak < w
+
+    return not falls, falls
+
+
 def parse_file(file, p2=False):
+    stack = np.fromregex(file, r'\d+', [('', int)]).reshape(-1, 6).astype(int)
+    stack = stack[stack[:, 2].argsort()] + [0, 0, 0, 1, 1, 1]
 
-    with open(file, 'r') as fp:
-        lines = [line for line in fp.read().splitlines()]
+    drop(stack)
 
-    MAX = 10
-
-    # Our bricks
-    B = defaultdict(int)
-    x, y, z = np.indices((MAX, MAX, MAX))
-
-    # Add our inputs
-    for i, line in enumerate(lines):
-        start, end = line.split('~')
-
-        ax, ay, az = map(int, start.split(','))
-        bx, by, bz = map(int, end.split(','))
-
-        (x1, x2) = (ax, bx) if ax < bx else (bx, ax)
-        (y1, y2) = (ay, by) if ay < by else (by, ay)
-        (z1, z2) = (az, bz) if az < bz else (bz, az)
-
-        B[i] = (x1 <= x) & (x <= x2) & (y1 <= y) & (y <= y2) & (z1 <= z) & (z <= z2)
-
-    # Create axis
-    axes = [MAX] * 3
-
-    # Control colour
-    colors = np.empty(axes, dtype=object)
-    COLORS = ['red', 'green', 'blue', 'yellow', 'purple', 'orange', 'pink']
-    for i in range(len(B)):
-        colors[B[i]] = COLORS[i % len(COLORS)]
-
-    # Voxels
-    voxelarray = reduce(np.logical_or, B.values())
-
-    # Plot figure
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.voxels(voxelarray, facecolors=colors, edgecolor='k')
-
-    plt.show()
-
-    return 0
+    res = np.sum([drop(stack.copy(), skip=i) for i in range(len(stack))], axis=0)
+    return res[1 if p2 else 0]
 
 
 def main():
     # Part 1
-    assert parse_file('test.txt') == 0
-    # print("Part 1: ", parse_file('input.txt'))
+    assert parse_file('test.txt') == 5
+    print("Part 1: ", parse_file('input.txt'))
 
     # Part 2
-    # assert parse_file('test.txt', True) == 0
-    # print("Part 2: ", parse_file('input.txt', True))
+    assert parse_file('test.txt', True) == 7
+    print("Part 2: ", parse_file('input.txt', True))
 
 
 if __name__ == "__main__":
     sys.exit(main())
-
-# %%
