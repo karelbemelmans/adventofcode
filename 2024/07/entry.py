c015d847b81a541e5d48ec7c27d9054000f7bd66
Karel Bemelmans <karel@narfum.org>
2024-12-07 09:41:22 +0100

    Added 2024 day 7 both parts

--- 2024/07/entry.py
+++ 2024/07/entry.py
@@ -5,7 +5,7 @@ from collections import deque
 
 
 # This is probably very ugly, but it works for now. 
-def calculate(numbers):
+def calculate(numbers, operators):
     Q = deque(numbers)
     
     # First number
@@ -17,13 +17,15 @@ def calculate(numbers):
     # if it's an operator or a number.
     while Q:
         x = Q.popleft()
-        if x in ['+', '*']:
+        if x in operators:
             op = x
         else:
             if op == '+':
                 T += x
             elif op == '*':
                 T *= x
+            elif op == '||':
+                T = int(f"%d%d" % (T,x))
 
     return T
     
@@ -32,17 +34,25 @@ def parse_file(file, p2=False):
     with open(file, 'r') as fp:
         lines = [line for line in fp.read().splitlines()]
 
+    # Gotta do it all in one line!
     pairs = [(int(a), [int(x) for x in b.split()]) for a, b in (line.strip().split(':') for line in lines)]
+    
+    if p2: 
+        operators = ['+', '*', '||']
+    else:
+        operators = ['+', '*']
     
     T = 0
     for total, numbers in pairs:
         
         # All possible combintations of operators for a list of numbers of this length
-        for ops in product('+*', repeat=len(numbers)-1):
+        for ops in product(operators, repeat=len(numbers)-1):
         
             # We merge both the numbers and operators list into one list
+            # Brute force, but it works for now :)
+            
             merged = sum(zip(numbers, list(ops)+[0]), ())[:-1]
-            t = calculate(merged)
+            t = calculate(merged, operators)
             if t == total:
                 T += t
                 break
@@ -55,5 +65,5 @@ assert parse_file('example.txt') == 3749
 print("Part 1: ", parse_file('input.txt'))
 
 # Part 2
-# assert parse_file('example.txt', True) == 31
-# print("Part 2: ", parse_file('input.txt', True))
+assert parse_file('example.txt', True) == 11387
+print("Part 2: ", parse_file('input.txt', True))
