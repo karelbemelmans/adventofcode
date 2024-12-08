743fc91ac2105e2d532ff49183eaf82f275d10c7
Karel Bemelmans <karel@narfum.org>
2024-12-08 11:07:02 +0100

    Added 2024 day 8 both parts

--- 2024/08/entry.py
+++ 2024/08/entry.py
@@ -4,40 +4,55 @@ from itertools import product, combinations
 from collections import deque
 
 
-def print_grid(grid):
-    dim = int(max(p.real for p in grid)) + 1
-
-    for y in range(dim):
-        print(''.join(grid[x+1j*y] for x in range(dim)))
-
-
-def calc_antinodes(grid, node):
+def calc_antinodes(grid, node, p2=False):
     antinodes = set()
 
     # Find all the antennas with the same frequency
     antennas = [x for x in grid if grid[x] == node]
+
+    # Maximum dimension of the grid
+    dim = int(max(p.real for p in grid)) + 1
 
     # We need more than 1 antenna to calculate antinodes
     if len(antennas) <= 1:
         return antinodes
 
     # Pair up antennas and calculate the antinodes:
-    #
-    #  In particular, an antinode occurs at any point that is perfectly in line with two antennas of the same frequency
-    #  - but only when one of the antennas is twice as far away as the other. This means that for any pair of antennas
-    #  with the same frequency, there are two antinodes, one on either side of them.
-
     combs = combinations(antennas, 2)
     for x, y in combs:
 
-        # Simple math to calculate the antinodes
-        a = 2*x - y
-        b = 2*y - x
+        # In p2 we calculate the line between the antennas and find all the points
+        if p2:
 
-        if a in grid:
-            antinodes.add(a)
-        if b in grid:
-            antinodes.add(b)
+            # Vector of the direction between the antennas
+            v = y - x
+
+            # Start at x, move in the direction of y
+            for i in range(0, dim):
+                p = x + i*v
+                if p in grid:
+                    antinodes.add(p)
+                else:
+                    break
+
+            # Start at x, move away from y
+            for i in range(0, dim):
+                p = x - i*v
+                if p in grid:
+                    antinodes.add(p)
+                else:
+                    break
+
+        # In p1 we calculate only 2 points and see if they are in the grid
+        else:
+            # Simple math to calculate the antinodes
+            a = 2*x - y
+            b = 2*y - x
+
+            if a in grid:
+                antinodes.add(a)
+            if b in grid:
+                antinodes.add(b)
 
     return antinodes
 
@@ -45,21 +60,18 @@ def calc_antinodes(grid, node):
 def parse_file(file, p2=False):
 
     # Let's do this one with complex numbers as in 2023 day 23
+    # Seeing how we need to do distance calculations that should make it very easy for us...
     with open(file, 'r') as fp:
         grid = {x+1j*y: c for y, l in enumerate(fp.read().splitlines()) for x, c in enumerate(l.strip())}
 
-    # Will contain coordinates of the antennas
+    # Unique coordinates of the antinodes
     T = set()
 
-    # Get the unique antennas in the grid
-    # Loop over them and calculate the antinodes
+    freqs = set([grid[x] for x in grid if grid[x] != '.'])
+    for freq in freqs:
+        antinodes = calc_antinodes(grid, freq, p2)
+        T.update(antinodes)
 
-    unique = set([grid[x] for x in grid if grid[x] != '.'])
-    for a in unique:
-        nodes = calc_antinodes(grid, a)
-        T.update(nodes)
-
-    print("Total antinodes: ", len(T))
     return len(T)
 
 
@@ -68,5 +80,5 @@ assert parse_file('example.txt') == 14
 print("Part 1: ", parse_file('input.txt'))
 
 # Part 2
-# assert parse_file('example.txt', True) == 11387
-# print("Part 2: ", parse_file('input.txt', True))
+assert parse_file('example.txt', True) == 34
+print("Part 2: ", parse_file('input.txt', True))
