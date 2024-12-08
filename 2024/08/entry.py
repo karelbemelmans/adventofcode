b01faa4b85878f472a44b86f6b0dcdecdc42bad9
Karel Bemelmans <karel@narfum.org>
2024-12-08 11:10:21 +0100

    Cleanup

--- 2024/08/entry.py
+++ 2024/08/entry.py
@@ -1,30 +1,19 @@
 #!/usr/bin/env python3
 
-from itertools import product, combinations
-from collections import deque
+from itertools import combinations
 
 
 def calc_antinodes(grid, node, p2=False):
     antinodes = set()
 
-    # Find all the antennas with the same frequency
     antennas = [x for x in grid if grid[x] == node]
-
-    # Maximum dimension of the grid
     dim = int(max(p.real for p in grid)) + 1
 
-    # We need more than 1 antenna to calculate antinodes
-    if len(antennas) <= 1:
-        return antinodes
-
-    # Pair up antennas and calculate the antinodes:
     combs = combinations(antennas, 2)
     for x, y in combs:
 
         # In p2 we calculate the line between the antennas and find all the points
         if p2:
-
-            # Vector of the direction between the antennas
             v = y - x
 
             # Start at x, move in the direction of y
@@ -45,7 +34,6 @@ def calc_antinodes(grid, node, p2=False):
 
         # In p1 we calculate only 2 points and see if they are in the grid
         else:
-            # Simple math to calculate the antinodes
             a = 2*x - y
             b = 2*y - x
 
@@ -59,12 +47,9 @@ def calc_antinodes(grid, node, p2=False):
 
 def parse_file(file, p2=False):
 
-    # Let's do this one with complex numbers as in 2023 day 23
-    # Seeing how we need to do distance calculations that should make it very easy for us...
     with open(file, 'r') as fp:
         grid = {x+1j*y: c for y, l in enumerate(fp.read().splitlines()) for x, c in enumerate(l.strip())}
 
-    # Unique coordinates of the antinodes
     T = set()
 
     freqs = set([grid[x] for x in grid if grid[x] != '.'])
