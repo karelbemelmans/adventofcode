cae74966d770ec647b86fc022ed1e4ba48f59ec0
Karel Bemelmans <karel.bemelmans@normative.io>
2024-12-09 16:13:31 +0100

    Added 2024 day 9 both parts

--- 2024/09/entry.py
+++ 2024/09/entry.py
@@ -12,7 +12,7 @@ def checksum(data):
     for x in data:
         if not x == ".":
             T += i * x
-            i += 1
+        i += 1
 
     return T
 
@@ -37,72 +37,71 @@ def explode(data):
     return output
 
 
-def compact(data, p2=False):
+def compact(data):
+    new = []
+    Q = deque(data)
 
-    if p2:
+    # We loop over the string from the start
+    # If we see an empty space, we pop an item off the end and replace it.
+    while Q:
+        item = Q.popleft()
+        if item == "." and Q:
 
-        # Split our array into groups with the same value aka file
-        G = deque([list(grp) for k, grp in groupby(data)])
-
-        k = 0
-        while True and k < len(G):
-            k += 1
-
-            last = G[-k]
-            print("last", last)
-
-            # We skip empty groups
-            if last.count("."):
-                print(G)
-                continue
-
-            for j, word in enumerate(G):
-                if word.count(".") >= len(last):
-                    print("found a spot for: ", last)
-
-                    # Remove this item from the end
-                    G.remove(last)
-
-                    # Add the 1 or 2 new items
-                    diff = len(word) - len(last)
-                    if diff:
-                        G[j] = ["."] * (len(word) - len(last))
-                        G.insert(j, last)
-
-                        # Our list increased with 1 item
-                        k -= 1
-                    else:
-                        G[j] = last
-
+            # Find the first non-dot char from the end
+            while Q:
+                pop = Q.pop()
+                if not pop == ".":
+                    new.append(pop)
                     break
-            else:
-                print("No spot found for: ", last)
+        else:
+            new.append(item)
 
-            print("new", G)
+    return new
 
-        G = list(reduce(lambda x, y: x + y, G, []))
-        return G
 
-    else:
-        new = []
-        Q = deque(data)
+def compact2(data):
 
-        # We loop over the string from the start
-        # If we see an empty space, we pop an item off the end and replace it.
-        while Q:
-            item = Q.popleft()
-            if item == "." and Q:
+    # Split our array into groups with the same value aka file
+    G = deque([list(grp) for k, grp in groupby(data)])
 
-                # Find the first non-dot char from the end
-                while Q:
-                    pop = Q.pop()
-                    if not pop == ".":
-                        new.append(pop)
-                        break
-            else:
-                new.append(item)
+    k = len(G)
 
-        return new
+    # This loop is ok since we need to try to move every word only once
+    # So there is no problem if there is still things to optimize in a 2nd or fruther pass
+    while True and k >= 0:
+        k -= 1
+        last = G[k]
+
+        # We skip empty groups
+        if last.count("."):
+            continue
+
+        for j, word in enumerate(G):
+
+            # Does it fit here AND it's on the left side were it currently is?
+            if word.count(".") >= len(last) and j < k:
+
+                # Add the 1 or 2 new items, depending how long the word is
+                diff = len(word) - len(last)
+                if diff:
+                    G[j] = ["."] * (len(word) - len(last))
+                    G.insert(j, last)
+
+                    G[k+1] = ["."] * len(last)
+
+                    # Our list has now increased with 1 item
+                    k += 1
+
+                # We can replace a while set of dots with our word
+                else:
+                    G[j] = last
+                    G[k] = ["."] * len(last)
+
+                # Move on the the next word
+                break
+
+    G = list(reduce(lambda x, y: x + y, G, []))
+    return G
 
 
 def parse_file(file, p2=False):
@@ -111,20 +110,21 @@ def parse_file(file, p2=False):
         data = [int(char) for char in fp.readline().strip()]
 
     data = explode(data)
-    print("Exploded: ", data)
 
-    data = compact(data, p2=p2)
-    print("Compacted: ", data)
+    if p2:
+        data = compact2(data)
+    else:
+        data = compact(data)
 
     T = checksum(data)
     return T
 
 
 # Part 1
-assert parse_file('example2.txt') == 60
 assert parse_file('example.txt') == 1928
+assert parse_file('example2.txt') == 60
 print("Part 1: ", parse_file('input.txt'))
 
 # Part 2
 assert parse_file('example.txt', True) == 2858
-# print("Part 2: ", parse_file('input.txt', True))
+print("Part 2: ", parse_file('input.txt', True))
