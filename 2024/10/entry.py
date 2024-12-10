ac96535375e421d2c80182456afa19688a599c46
Karel Bemelmans <karel@narfum.org>
2024-12-10 08:43:37 +0100

    Added 2024 day 10 both parts

--- 2024/10/entry.py
+++ 2024/10/entry.py
@@ -3,22 +3,18 @@
 import heapq
 
 
-def trailhead(G, start, end):
+def nr_of_paths(G, start, end):
 
     Q = [(0, *start)]
-    S = set()
+    count = 0
 
     while Q:
         l, r, c = heapq.heappop(Q)
 
         # We reached the end? Done.
         if (r, c) == end:
-            return True
-
-        if (r, c) in S:
+            count += 1
             continue
-
-        S.add((r, c))
 
         # We can only go in 4 directions, no diagonals
         for dr, dc in {(1, 0), (0, 1), (-1, 0), (0, -1)}:
@@ -32,44 +28,40 @@ def trailhead(G, start, end):
             if (a, b) in G and G[a, b] == G[r, c] + 1:
                 heapq.heappush(Q, (l+1, a, b))
 
-    return False
+    return count
 
 
 def parse_file(file, p2=False):
 
-    # Different appreach with tuples for a grid
-    # End point can simply by selected with using max(G)
+    # We like complex numbers now.
     with open(file, 'r') as fp:
         G = {(i, j): int(c) for i, row in enumerate(fp.read().splitlines())
              for j, c in enumerate(row.strip())}
 
-    print(G)
-
     starts = [x for x in G if G[x] == 0]
     ends = [x for x in G if G[x] == 9]
-    print(starts, ends)
 
     T = 0
+
+    # Simple counting the nr of paths from all starts to all ends
     for s in starts:
-        print("Start: ", s)
-        t = 0
         for e in ends:
 
-            if trailhead(G, s, e):
-                t += 1
+            # In p2 we look at the nr of paths
+            if p2:
+                T += nr_of_paths(G, s, e)
 
-        print("This point has paths: ", t)
-        T += t
+            # In p1 we only look if there is a path possible
+            else:
+                T += 1 if nr_of_paths(G, s, e) else 0
 
-    print("T", T)
     return T
 
 
 # Part 1
-assert parse_file('example2.txt') == 1
 assert parse_file('example.txt') == 36
 print("Part 1: ", parse_file('input.txt'))
 
 # Part 2
-# assert parse_file('test.txt', True) == 8
-# print("Part 2: ", parse_file('input.txt', True))
+assert parse_file('example.txt', True) == 81
+print("Part 2: ", parse_file('input.txt', True))
