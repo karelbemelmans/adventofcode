176c5adbb8781c04bb6195315262ebe7faea9ac1
Karel Bemelmans <karel@narfum.org>
2024-12-13 08:52:08 +0100

    Added 2024 day 11 both parts

--- 2024/11/entry.py
+++ 2024/11/entry.py
@@ -1,39 +1,40 @@
 #!/usr/bin/env python3
 
-def blink(numbers):
-    new = []
 
-    """
-    - If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
-    - If the stone is engraved with a number that has an even number of digits, it is replaced by two stones. 
-        - The left half of the digits are engraved on the new left stone, 
-        - and the right half of the digits are engraved on the new right stone. (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
-    - If none of the other rules apply, the stone is replaced by a new stone; the old stone's number multiplied by 2024 is engraved on the new stone.
-   """
-
-    for n in numbers:
-        if n == 0:
-            new.append(1)
-        elif len(str(n)) % 2 == 0:
-            new.append(int(str(n)[:len(str(n))//2]))
-            new.append(int(str(n)[len(str(n))//2:]))
-        else:
-            new.append(n * 2024)
-
-    return new
+# Gold old dynamic programming cache
+DP = {}
 
 
-def parse_file(input, count=0, p2=False):
-    numbers = [int(x) for x in input.split()]
+def blink(x, t):
 
-    print(numbers)
+    # Cached?
+    if (x, t) in DP:
+        return DP[(x, t)]
 
-    for i in range(count):
-        numbers = blink(numbers)
-        print("i: ",  numbers)
+    # Done?
+    if t == 0:
+        T = 1
 
-    T = len(numbers)
+    # Parse our rules
+    elif x == 0:
+        T = blink(1, t-1)
+
+    elif len(str(x)) % 2 == 0:
+        sx = str(x)
+        left = int(sx[:len(sx)//2])
+        right = int(sx[len(sx)//2:])
+        T = blink(left, t-1) + blink(right, t-1)
+
+    else:
+        T = blink(x*2024, t-1)
+
+    DP[(x, t)] = T
     return T
+
+
+def parse_file(input, count=6):
+    numbers = [int(x) for x in input.split()]
+    return sum(blink(n, count) for n in numbers)
 
 
 # Part 1
@@ -44,5 +45,4 @@ assert parse_file('125 17', 25) == 55312
 print("Part 1: ", parse_file('0 7 6618216 26481 885 42 202642 8791', 25))
 
 # Part 2
-# assert parse_file('example.txt', True) == 11387
-# print("Part 2: ", parse_file('input.txt', True))
+print("Part 1: ", parse_file('0 7 6618216 26481 885 42 202642 8791', 75))
