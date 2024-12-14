6ba111d579f5cf68fa946ff8b042853ba6d0b9c8
Karel Bemelmans <karel@narfum.org>
2024-12-14 17:34:59 +0100

    Added 2024 day 14 both parts

--- 2024/14/entry.py
+++ 2024/14/entry.py
@@ -3,22 +3,19 @@
 import sys
 
 
+def we_have_a_tree(robots):
+    # Thanks to the reddit tip: "We probably have a tree if there is only max 1 robot per position"
+    # This means all the items in our list have to be unique, for which we use a set()
+    return len(robots) == len(set(robots))
+
+
 def score(X, Y, robots):
-    T = 1
+    TL = sum([robots.count((x, y)) for x in range(0, X // 2) for y in range(0, Y//2)])
+    TR = sum([robots.count((x, y)) for x in range(X // 2, X) for y in range(0, Y//2)])
+    BL = sum([robots.count((x, y)) for x in range(0, X // 2) for y in range(Y//2 + 1, Y)])
+    BR = sum([robots.count((x, y)) for x in range(X // 2 + 1, X) for y in range(Y//2 + 1, Y)])
 
-    # Top left
-    T *= sum([robots.count((x, y)) for x in range(0, X // 2) for y in range(0, Y//2)])
-
-    # Top right
-    T *= sum([robots.count((x, y)) for x in range(X // 2, X) for y in range(0, Y//2)])
-
-    # Bottom left
-    T *= sum([robots.count((x, y)) for x in range(0, X // 2) for y in range(Y//2 + 1, Y)])
-
-    # Bottom right
-    T *= sum([robots.count((x, y)) for x in range(X // 2 + 1, X) for y in range(Y//2 + 1, Y)])
-
-    return T
+    return TL * TR * BL * BR
 
 
 def print_robots_grid(X, Y, robots):
@@ -32,41 +29,39 @@ def print_robots_grid(X, Y, robots):
         print()
 
 
-def parse_file(file, X, Y, S=10, p2=False):
+def parse_file(file, X, Y, p2=False):
 
     with open(file, 'r') as fp:
         robots = [[tuple([int(y) for y in x[2:].split(",")]) for x in line.split(" ")]
                   for line in fp.read().splitlines()]
 
-    # print_robots_grid(X, Y, [x[0] for x in robots])
+    S = 1
+    while True:
 
-    # Loop over robots
-    for i in range(len(robots)):
-
-        # Loop for S seconds
-        for s in range(1, S+1):
-            # print("Second: ", s)
-
-            # TODO: Write this a bit more readable
+        # Move all our robots
+        for i in range(len(robots)):
             robots[i][0] = ((robots[i][0][0] + robots[i][1][0]) % X, (robots[i][0][1] + robots[i][1][1]) % Y)
 
-    # Final state
-    print_robots_grid(X, Y, [x[0] for x in robots])
+        if p2:
+            if we_have_a_tree([x[0] for x in robots]):
+                print_robots_grid(X, Y, [x[0] for x in robots])
+                return S
 
-    T = score(X, Y, [x[0] for x in robots])
+        else:
+            if S == 100:
+                return score(X, Y, [x[0] for x in robots])
 
-    print("T: ", T)
-    return T
+        S += 1
 
 
 def main():
     # Part 1
-    # assert parse_file('example2.txt', 11, 7, 10) == 12
-    assert parse_file('example.txt', 11, 7, 100) == 12
-    print("Part 1: ", parse_file('input.txt', 101, 103, 100))
+    assert parse_file('example.txt', 11, 7) == 12
+    print("Part 1: ", parse_file('input.txt', 101, 103))
 
     # Part 2
-    # print("Part 2: ", parse_file('input.txt', True))
+    # assert parse_file('example.txt', 11, 7, True) == 12
+    print("Part 2: ", parse_file('input.txt', 101, 103, True))
 
 
 if __name__ == "__main__":
