ebaecc06594300c95374ad018f5856aa3bedf798
Karel Bemelmans <karel.bemelmans@normative.io>
2024-12-20 21:12:31 +0100

    Added 2024 day 15 part 1

--- 2024/15/entry.py
+++ 2024/15/entry.py
@@ -14,19 +14,20 @@ def print_grid(grid):
 def parse_file(file, p2=False):
 
     with open(file, 'r') as fp:
-        data, moves = fp.read().split("\n\n")
+        a, b = fp.read().split("\n\n")
 
     # This should be doable in one run as well....
-    grid = {i+j*1j: c for i, row in enumerate(data.splitlines()) for j, c in enumerate(row.strip())}
+    grid = {i+j*1j: c for i, row in enumerate(a.splitlines()) for j, c in enumerate(row.strip())}
+    moves = "".join(i.replace("\n", "") for i in b)
 
-    print_grid(grid)
+    print(grid, moves)
 
     T = 0
     cursor = [k for k in grid if grid[k] == '@'][0]
     print("cursor: ", cursor)
     DIR = {'^': -1, '>': 1j, 'v': 1, '<': -1j}
 
-    for m in moves.strip():
+    for m in moves:
         next = cursor + DIR[m]
         print("move, next: ", m, next)
 
@@ -38,21 +39,42 @@ def parse_file(file, p2=False):
 
         # We hit a box
         elif next in grid and grid[next] == 'O':
-            a = 1
 
-        # We are hitting a wall - do nothing
-        elif next in grid and grid[next] == '#':
-            b = 1
+            # Is there a dot in the direction we are going?
+            step = next
 
-        print_grid(grid)
+            while True:
+                step += DIR[m]
+                match grid[step]:
 
+                    # We have a free spot
+                    case '.':
+                        # Move boxes
+                        grid[step] = 'O'
+
+                        # Move our cursor
+                        grid[cursor] = '.'
+                        grid[next] = '@'
+                        cursor = next
+                        break
+
+                    # We hit a wall, we are done
+                    case '#':
+                        break
+
+                    # Other spots can only be boxes, we ignore those
+
+        # print_grid(grid)
+
+    T = sum(int(k.real * 100 + k.imag) for k in grid if grid[k] == 'O')
+    print("T: ", T)
     return T
 
 
 # Part 1
 assert parse_file('example.txt') == 2028
-# assert parse_file('example2.txt') == 10092
-# print("Part 1: ", parse_file('input.txt'))
+assert parse_file('example2.txt') == 10092
+print("Part 1: ", parse_file('input.txt'))
 
 # Part 2
 # assert parse_file('example.txt', True) == 300
