795de2ebe83f67df69e78d90a06aa67a2cde91bd
Karel Bemelmans <karel.bemelmans@normative.io>
2025-12-01 18:59:32 +0100

    Added 2025 day 1

--- 2025/01/entry.py
+++ 2025/01/entry.py
@@ -3,23 +3,38 @@
 
 def parse_file(file, p2=False):
     with open(file, "r") as fp:
-        lines = [line for line in fp.read().splitlines()]
+        steps = [(line[0], int(line[1:])) for line in fp.read().splitlines()]
 
-    # Read lines into pairs of integers
-    steps = [(line[0], int(line[1:]) for line in lines]
-    print((steps)
-    
+    cur = 50
+    direction = "R"
+    hits = 0
+    M = 100
 
-    if p2:
-        return sum([a * second.count(a) for a in first])
-    else:
-        return sum([abs(first[i] - second[i]) for i in range(0, len(pairs))])
+    for step in steps:
+        letter = step[0]
+        number = step[1]
+
+        if letter != direction:
+            cur = (M - cur) % M
+            direction = letter
+
+        cur += number
+
+        if p2:
+            hits += cur // M
+            cur = cur % M
+        elif cur % M == 0:
+            hits += 1
+
+    print("Final counter: ", hits)
+    return hits
 
 
 # Part 1
-assert parse_file("example.txt") == 11
+assert parse_file("example.txt") == 3
 print("Part 1: ", parse_file("input.txt"))
 
 # Part 2
-assert parse_file("example.txt", True) == 31
+assert parse_file("example.txt", True) == 6
+assert parse_file("example2.txt", True) == 10
 print("Part 2: ", parse_file("input.txt", True))
