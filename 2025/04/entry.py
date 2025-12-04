2a43ed9a8752d17855ce16bc1b1653ffcf21041c
Karel Bemelmans <karel.bemelmans@normative.io>
2025-12-04 08:09:46 +0100

    Added 2025 day 4 part 1

---
+++ 2025/04/entry.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+
+# The forklifts can only access a roll of paper if there are fewer
+# than four rolls of paper in the eight adjacent positions.
+def can_access(grid, p):
+    directions = (1 + 1j, 1, 1 - 1j, 0 + 1j, 0 - 1j, -1 + 1j, -1, -1 - 1j)
+
+    # Find all neighbours of p
+    P = {p + d for d in directions if p + d in grid}
+
+    return len(P) < 4
+
+
+def parse_file(file, p2=False):
+
+    # We use imaginary numbers to create an (x,y) grid
+    # This allows us to easily add up points
+    with open(file, "r") as fp:
+        grid = {
+            x + 1j * y: c
+            for y, l in enumerate(fp.read().splitlines())
+            for x, c in enumerate(l.strip())
+        }
+
+    # Keep only the points where we have a roll
+    rolls = [x for x in grid if grid[x] == "@"]
+
+    T = 0
+    start = None
+
+    for r in rolls:
+        if can_access(rolls, r):
+            T += 1
+
+    # print(T)
+    return T
+
+
+# Part 1
+assert parse_file("example.txt") == 13
+print("Part 1: ", parse_file("input.txt"))
+
+# Part 2
+# assert parse_file('example.txt', True) == 6
+# print("Part 2: ", parse_file('input.txt', True))
