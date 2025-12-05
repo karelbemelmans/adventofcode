fc8d152b82b3a2965ed07c80e52963acf138c28e
Karel Bemelmans <karel.bemelmans@normative.io>
2025-12-05 13:44:29 +0100

    cleanup

--- 2025/04/entry.py
+++ 2025/04/entry.py
@@ -1,33 +1,25 @@
 #!/usr/bin/env python3
 
 
-def can_access(rolls_set, p):
+def can_access(R, p):
     directions = (1 + 1j, 1, 1 - 1j, 0 + 1j, 0 - 1j, -1 + 1j, -1, -1 - 1j)
-
-    # Count neighbors that are rolls
-    neighbor_count = sum(1 for d in directions if (p + d) in rolls_set)
-
-    return neighbor_count < 4
+    return sum(1 for d in directions if (p + d) in R) < 4
 
 
 def parse_file(file, p2=False):
     with open(file, "r") as fp:
-        grid = {
-            x + 1j * y: c
+        # Use a set for O(1) lookups and removals
+        rolls = {
+            x + 1j * y
             for y, l in enumerate(fp.read().splitlines())
             for x, c in enumerate(l.strip())
+            if c == "@"
         }
 
-    # Use a set for O(1) lookups and removals
-    rolls = {x for x in grid if grid[x] == "@"}
     start = len(rolls)
 
     if p2:
-        i = 0
         while True:
-            # print("run: %d, length of rolls: %d" % (i, len(rolls)))
-            i += 1
-
             # Find all removable rolls in current iteration
             to_remove = {r for r in rolls if can_access(rolls, r)}
 
