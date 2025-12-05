8c0a53ce6d1e7cb87a8486524501940d09daa07d
Karel Bemelmans <karel.bemelmans@normative.io>
2025-12-05 08:41:34 +0100

    cleanup

--- 2025/05/entry.py
+++ 2025/05/entry.py
@@ -9,30 +9,26 @@ def parse_file(file, p2=False):
     F = sorted(
         {(int(a), int(b)) for a, b in [rule.split("-") for rule in fresh.splitlines()]}
     )
-    T = 0
 
     if p2:
-
-        # Merge all the overlapping ranges into a new range
-        reduced = reduce(
-            lambda m, x: (
-                m[:-1] + [[m[-1][0], max(m[-1][1], x[1])]]
-                if m and x[0] <= m[-1][1] + 1
-                else m + [x]
-            ),
-            F,
-            [],
+        return sum(
+            end - start + 1
+            # Merge all the overlapping ranges into a new range
+            for start, end in reduce(
+                lambda m, x: (
+                    m[:-1] + [[m[-1][0], max(m[-1][1], x[1])]]
+                    if m and x[0] <= m[-1][1] + 1
+                    else m + [x]
+                ),
+                F,
+                [],
+            )
         )
-
-        T = sum(end - start + 1 for start, end in reduced)
 
     else:
-        T = sum(
+        return sum(
             any(f[0] <= int(x) <= f[1] for f in F) for x in ingredients.splitlines()
         )
-
-    print(T)
-    return T
 
 
 # Part 1
