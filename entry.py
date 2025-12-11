f17e5878bfea003491fb8e30ec7566fb7b46c0f9
Karel Bemelmans <karel.bemelmans@normative.io>
2025-12-11 22:21:08 +0100

    Use a normal requirements.txt setup

--- entry.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-if __name__ == "__main__":
-    if sys.argv[1] == "run":
-        year, day = sys.argv[2], sys.argv[3]
-        print("Running: %s %s" % (year, day))
-
-    else:
-        raise Exception(f"Invalid command: {sys.argv[1]}")
